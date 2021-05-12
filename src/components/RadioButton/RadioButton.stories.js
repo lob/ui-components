c8bedbb92b76f2f6d93d599a3371f74389b4f0f5
Beth Qiang <beth.qiang@lob.com>
2021-05-12 13:43:53 -0600

    edit separate line behavior, add radio to storybook

---
+++ src/components/RadioButton/RadioButton.stories.js
@@ -0,0 +1,32 @@
+import RadioButton from './RadioButton.vue';
+import mdx from './RadioButton.mdx';
+
+export default {
+  title: 'Components/Radio Button',
+  component: RadioButton,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  },
+  argTypes: {
+    'v-model': {
+      control: {
+        type: null
+      }
+    }
+  }
+};
+
+const Template = (args, {argTypes}) => ({
+  props: Object.keys(argTypes),
+  components: { RadioButton },
+  template: '<radio-button v-bind="$props" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  name: 'postcard-size',
+  label: '4x6',
+  value: '4x6',
+}
---
+++ src/components/RadioGroup/RadioGroup.stories.js
@@ -0,0 +1,44 @@
+import RadioGroup from './RadioGroup.vue';
+import RadioButton from '../RadioButton/RadioButton.vue';
+import mdx from './RadioGroup.mdx';
+
+export default {
+  title: 'Components/Radio Group',
+  component: RadioGroup,
+  subcomponents: { RadioButton },
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  },
+};
+
+const Template = (args, {argTypes}) => ({
+  props: Object.keys(argTypes),
+  components: { RadioGroup, RadioButton },
+  template: `
+    <radio-group v-bind="$props">
+      <radio-button
+        name="postcard-size"
+        label="4x6"
+        value="4x6"
+      />
+      <radio-button
+        name="postcard-size"
+        label="5x7"
+        value="5x7"
+      />
+      <radio-button
+        name="postcard-size"
+        label="Really Big"
+        value="big"
+      />
+    </radio-group>
+  `,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  legend: 'Postcard Size',
+  separateLines: false,
+}
