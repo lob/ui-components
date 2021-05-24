f2aee4d77f12b31677e8d60212ef04b626988e29
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-05-24 12:18:09 -0500

    created a Theme/Colors story to make it easy to see our configured colors

---
+++ src/theme/colors/Colors.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <div
+        v-for="color in graidentColors"
+        :key="color"
+        class="pb-8"
+      >
+        <h1>{{ color }}</h1>
+        <h2 :class="`text-${color}`">
+          {{ color }}
+        </h2>
+        <div
+          v-for="modifier in modifiers"
+          :key="`${color}-${modifier}`"
+        >
+          <h2 :class="`text-${color}-${modifier}`">
+            {{ color }}-{{ modifier }}
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div
+      v-for="color in singleColors"
+      :key="color"
+      class="pb-8"
+    >
+      <h1>{{ color }}</h1>
+      <h2 :class="`text-${color}`">
+        {{ color }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { theme } from '../../../tailwind.config';
+
+const { colors } = theme;
+
+export default {
+  name: 'Theme',
+  computed: {
+    singleColors () {
+      return Object.keys(colors).reduce((singles, color) => {
+        if (typeof colors[color] === 'string') {
+          singles.push(color);
+        }
+        return singles;
+      }, []);
+    },
+    graidentColors () {
+      return Object.keys(colors).reduce((gradients, color) => {
+        if (typeof colors[color] === 'object') {
+          gradients.push(color);
+        }
+        return gradients;
+      }, []);
+    },
+    modifiers () {
+      return ['xd', 'd', 'l', 'xl'];
+    }
+  }
+};
+</script>
