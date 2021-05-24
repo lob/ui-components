cbc2bd437dbaffe0f8a06a23386e705becba6951
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-05-24 12:26:15 -0500

    created a Theme/FontSizes story to make it easy to see our configured text sizes

---
+++ src/theme/fontSize/FontSize.stories.js
@@ -0,0 +1,20 @@
+import FontSize from './FontSize.vue';
+import mdx from './FontSize.mdx';
+
+export default {
+  title: 'Theme/FontSize',
+  component: FontSize,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FontSize },
+  template: '<font-size></font-size>'
+});
+
+export const Primary = Template.bind({});
