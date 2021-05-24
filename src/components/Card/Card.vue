ff2cfeb9cfc68bc34c5b09a472e8e6c6ecb20814
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-05-24 15:29:44 -0500

    style Card with tailwind

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card p-6">
     <slot name="content" />
   </div>
 </template>
@@ -12,7 +12,6 @@ export default {
 
 <style scoped lang="scss">
   .card {
-    padding: 24px;
     box-shadow: 0 5px 14px rgba(44, 67, 81, 0.13), 0 0 4px rgba(44, 67, 81, 0.02);
   }
 </style>
