437438df3e4af41ca20f2704d7a071f935ed6044
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-05-27 12:58:41 -0500

    test for link or button

--- src/components/MainNavigation/__tests__/MainNavigationItem.spec.js
+++ src/components/MainNavigation/__tests__/MainNavigationItem.spec.js
@@ -1,7 +1,14 @@
 import '@testing-library/jest-dom';
+import VueRouter from 'vue-router';
+
 import { render, fireEvent, waitFor } from '@testing-library/vue';
 import { constants } from '../../../config';
 import MainNavigationItem from '../MainNavigationItem.vue';
+
+const configureVue = (vue) => {
+  vue.use(VueRouter);
+  vue.use(constants);
+};
 
 const initialProps = {
   title: 'Overview',
@@ -10,10 +17,9 @@ const initialProps = {
   active: false,
   collapsed: false
 };
-const configureVue = (vue) => {
-  vue.use(constants);
-};
-const renderComponent = (options) => render(MainNavigationItem, options, configureVue);
+
+const router = new VueRouter({ mode: 'history' });
+const renderComponent = (options) => render(MainNavigationItem, { ...options, router }, configureVue);
 
 describe('Main Navigation Item', () => {
 
@@ -28,114 +34,147 @@ describe('Main Navigation Item', () => {
     expect(image).toHaveAttribute('src', props.iconSrc);
   });
 
-  describe('without chld nav items', () => {
+  describe('with a to prop', () => {
 
-    it('does not render a collapse/expand icon', () => {
-      const props = initialProps;
-      const { queryByAltText } = renderComponent({ props });
+    it('renders a link', () => {
+      const props = {
+        ...initialProps,
+        to: '/overview'
+      };
+      const { queryByRole } = renderComponent({ props });
 
-      const image = queryByAltText(/Collapse|Expand/i);
-      expect(image).not.toBeInTheDocument();
+      const link = queryByRole('link');
+      expect(link).toBeInTheDocument();
+
+      const button = queryByRole('button');
+      expect(button).not.toBeInTheDocument();
     });
 
   });
 
-  describe('with child nav items', () => {
+  describe('without a to prop', () => {
 
-    let slots;
-    let slotContent;
-
-    beforeEach(() => {
-      slotContent = 'I\'m a child';
-      slots = { default: [`<ul><li>${slotContent}</li></ul>`] };
-    });
-
-    it('renders a collapse/expand icon', () => {
+    it('renders a button', () => {
       const props = initialProps;
-      const { queryByAltText } = renderComponent({ props, slots });
+      const { queryByRole } = renderComponent({ props });
 
-      const image = queryByAltText(/Collapse|Expand/i);
-      expect(image).toBeInTheDocument();
+      const link = queryByRole('link');
+      expect(link).not.toBeInTheDocument();
+
+      const button = queryByRole('button');
+      expect(button).toBeInTheDocument();
     });
 
-    it('renders the slot content', () => {
-      const props = initialProps;
-      const { queryByText } = renderComponent({ props, slots });
+    describe('without chld nav items', () => {
 
-      const slot = queryByText(slotContent);
-      expect(slot).toBeInTheDocument();
-    });
+      it('does not render a collapse/expand icon', () => {
+        const props = initialProps;
+        const { queryByAltText } = renderComponent({ props });
 
-    describe('and expanded', () => {
-
-      let props;
-
-      beforeEach(() => {
-        props = {
-          ...initialProps,
-          collapsed: false
-        };
+        const image = queryByAltText(/Collapse|Expand/i);
+        expect(image).not.toBeInTheDocument();
       });
 
-      it('renders a collapse icon', () => {
+    });
+
+    describe('with child nav items', () => {
+
+      let slots;
+      let slotContent;
+
+      beforeEach(() => {
+        slotContent = 'I\'m a child';
+        slots = { default: [`<ul><li>${slotContent}</li></ul>`] };
+      });
+
+      it('renders a collapse/expand icon', () => {
+        const props = initialProps;
         const { queryByAltText } = renderComponent({ props, slots });
 
-        const image = queryByAltText(/Collapse/i);
+        const image = queryByAltText(/Collapse|Expand/i);
         expect(image).toBeInTheDocument();
       });
 
-      it('collapses the sub navigation when clicked', async () => {
-        const { getByText, queryByText, queryByAltText } = renderComponent({ props, slots });
+      it('renders the slot content', () => {
+        const props = initialProps;
+        const { queryByText } = renderComponent({ props, slots });
 
-        const button = getByText(props.title);
-        fireEvent.click(button);
-
-        await waitFor(() => {
-          const slot = queryByText(slotContent);
-          expect(slot).not.toBeInTheDocument();
-        });
-
-        await waitFor(() => {
-          const image = queryByAltText(/Expand/i);
-          expect(image).toBeInTheDocument();
-        });
+        const slot = queryByText(slotContent);
+        expect(slot).toBeInTheDocument();
       });
 
-    });
+      describe('and expanded', () => {
 
-    describe('and collapsed', () => {
+        let props;
 
-      let props;
-
-      beforeEach(() => {
-        props = {
-          ...initialProps,
-          collapsed: true
-        };
-      });
-
-      it('renders an expand icon', () => {
-        const { queryByAltText } = renderComponent({ props, slots });
-
-        const image = queryByAltText(/Expand/i);
-        expect(image).toBeInTheDocument();
-      });
-
-      it('expands the sub navigation when clicked', async () => {
-        const { getByText, queryByText, queryByAltText } = renderComponent({ props, slots });
-
-        const button = getByText(props.title);
-        fireEvent.click(button);
-
-        await waitFor(() => {
-          const slot = queryByText(slotContent);
-          expect(slot).toBeInTheDocument();
+        beforeEach(() => {
+          props = {
+            ...initialProps,
+            collapsed: false
+          };
         });
 
-        await waitFor(() => {
+        it('renders a collapse icon', () => {
+          const { queryByAltText } = renderComponent({ props, slots });
+
           const image = queryByAltText(/Collapse/i);
           expect(image).toBeInTheDocument();
         });
+
+        it('collapses the sub navigation when clicked', async () => {
+          const { getByText, queryByText, queryByAltText } = renderComponent({ props, slots });
+
+          const button = getByText(props.title);
+          fireEvent.click(button);
+
+          await waitFor(() => {
+            const slot = queryByText(slotContent);
+            expect(slot).not.toBeInTheDocument();
+          });
+
+          await waitFor(() => {
+            const image = queryByAltText(/Expand/i);
+            expect(image).toBeInTheDocument();
+          });
+        });
+
+      });
+
+      describe('and collapsed', () => {
+
+        let props;
+
+        beforeEach(() => {
+          props = {
+            ...initialProps,
+            collapsed: true
+          };
+        });
+
+        it('renders an expand icon', () => {
+          const { queryByAltText } = renderComponent({ props, slots });
+
+          const image = queryByAltText(/Expand/i);
+          expect(image).toBeInTheDocument();
+        });
+
+        it('expands the sub navigation when clicked', async () => {
+          const { getByText, queryByText, queryByAltText } = renderComponent({ props, slots });
+
+          const button = getByText(props.title);
+          fireEvent.click(button);
+
+          await waitFor(() => {
+            const slot = queryByText(slotContent);
+            expect(slot).toBeInTheDocument();
+          });
+
+          await waitFor(() => {
+            const image = queryByAltText(/Collapse/i);
+            expect(image).toBeInTheDocument();
+          });
+        });
+
       });
 
     });
