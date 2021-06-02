993311249f62c4e62320698ceb7dd8d50e6faad3
Mercedes <mercedesb@users.noreply.github.com>
2021-06-02 15:27:17 -0500

    Dang 128/switch component (#22)
    * create switch component for live/test toggle
    
    * add switch docs
    
    * add default switch checked state to story
    
    * rename spec file

---
+++ src/components/Switch/__tests__/SwitchGroup.spec.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import SwitchGroup from '../SwitchGroup.vue';
+
+const initialProps = {
+  legend: 'legend',
+  srOnlyLegend: true
+};
+
+describe('Switch Group', () => {
+
+  it('renders a legend for accessibility', () => {
+    const props = initialProps;
+    const { queryByText } = render(SwitchGroup, {
+      props
+    });
+
+    const legend = queryByText(props.legend);
+    expect(legend).toBeInTheDocument();
+  });
+
+});
