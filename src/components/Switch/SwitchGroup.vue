30a8a686f31f99ff0f4e1be5e2a61bc69552d753
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-02 10:25:36 -0500

    create switch component for live/test toggle

---
+++ src/components/Switch/SwitchGroup.vue
@@ -0,0 +1,35 @@
+<template>
+  <fieldset
+    role="radiogroup"
+  >
+    <legend :class="['text-sm font-normal normal-case tracking-normal text-gray-500 mb-1 border-b-0', {'sr-only': srOnlyLegend}]">
+      {{ legend }}
+    </legend>
+    <div class="lob-switch-group flex p-1 bg-white rounded">
+      <slot />
+    </div>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'SwitchGroup',
+  props: {
+    legend: {
+      type: String,
+      required: true
+    },
+    srOnlyLegend: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .lob-switch-group {
+    box-shadow: 0 5px 14px rgba(44, 67, 81, 0.13), 0 0 4px rgba(44, 67, 81, 0.02);
+  }
+</style>
+
---
+++ src/components/Switch/SwitchItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    :class="[
+      'rounded flex',
+      {'bg-white text-gray-500': !checked},
+      {'bg-primary-500 text-white checked': checked},
+    ]"
+  >
+    <input
+      :id="value"
+      type="radio"
+      class="sr-only pointer-events-none"
+      :name="name"
+      :value="value"
+      :checked="checked"
+      :disabled="disabled"
+      @input="onInput"
+      @click="onClick"
+    >
+    <label
+      :for="value"
+      class="px-6 py-1.5 cursor-pointer"
+    >{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwitchItem',
+  model: {
+    prop: 'modelValue',
+    event: 'input'
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: null
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    checked () {
+      return this.modelValue === this.value;
+    }
+  },
+  methods: {
+    onInput () {
+      this.$emit('input', this.value);
+    },
+    onClick ($event) {
+      this.$emit('click', $event);
+    }
+  }
+};
+
+</script>
+
