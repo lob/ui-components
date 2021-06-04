00822f065f76e3571a7c710411e4eec2e2e8aff0
Mercedes <mercedesb@users.noreply.github.com>
2021-06-04 11:42:33 -0500

    Dang 133/tooltip (#25)
    * initial tooltip and styles
    
    * hookup hover events on trigger slot
    
    * dynamically calculate position of tooltip based on described content
    
    * add docs
    
    * emit mouse events
    
    * add specs
    
    * move story control options to correct level for Storybookv7+
    
    * make arrow placement default center instead of empty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+var flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
+
 module.exports = {
   purge: [
     './src/**/*.vue'
@@ -119,5 +121,21 @@ module.exports = {
       translate: ['group-hover']
     }
   },
-  plugins: []
+  plugins: [
+    ({ addUtilities, theme, variants }) => {
+      const colors = flattenColorPalette(theme('borderColor'));
+      delete colors.default;
+
+      const colorMap = Object.keys(colors)
+        .map((color) => ({
+          [`.border-t-${color}`]: { borderTopColor: colors[color] },
+          [`.border-r-${color}`]: { borderRightColor: colors[color] },
+          [`.border-b-${color}`]: { borderBottomColor: colors[color] },
+          [`.border-l-${color}`]: { borderLeftColor: colors[color] }
+        }));
+      const utilities = Object.assign({}, ...colorMap);
+
+      addUtilities(utilities, variants('borderColor'));
+    }
+  ]
 };
