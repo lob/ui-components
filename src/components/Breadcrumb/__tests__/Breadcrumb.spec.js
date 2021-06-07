c3fe2266f907a823dbde1acf123a7a0e9a528072
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-07 16:54:38 -0500

    update specs with new VTL syntax for vue 3

--- src/components/Breadcrumb/__tests__/Breadcrumb.spec.js
+++ src/components/Breadcrumb/__tests__/Breadcrumb.spec.js
@@ -1,69 +1,79 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/vue';
-import Breadcrumb from '../Breadcrumb.vue';
+import "@testing-library/jest-dom";
+import { createRouter, createMemoryHistory } from "vue-router";
+import { render } from "@testing-library/vue";
+import Breadcrumb from "../Breadcrumb.vue";
 
 const initialProps = {
-  startName: 'Dashboard'
-};
-
-const configureWithCurrentRoute = (_vue, _store, router) => {
-  router.push('/envelopes/create');
+  startName: "Dashboard",
 };
 
 const routes = [
+  { path: "" },
+  { path: "/" },
   {
-    path: '/envelopes',
-    name: 'Envelopes',
+    path: "/envelopes",
+    name: "Envelopes",
     component: {
-      template: '<div>envelopes</div>'
+      template: "<div>envelopes</div>",
     },
     children: [
       {
-        path: 'create',
+        path: "create",
         component: {
-          template: '<div>create</div>'
-        }
-      }
-    ]
-  }
+          template: "<div>create</div>",
+        },
+      },
+    ],
+  },
 ];
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
 
-const renderComponent = (options, configure = configureWithCurrentRoute) => render(Breadcrumb, { ...options, routes }, configure);
+const renderComponent = (options) =>
+  render(Breadcrumb, { ...options, global: { plugins: [router] } });
 
-describe('Breadcrumb', () => {
-
-  it('renders a semantic nav element', () => {
+describe("Breadcrumb", () => {
+  it("renders a semantic nav element", async () => {
     const props = initialProps;
     const { queryByRole } = renderComponent({ props });
+    router.push("/envelopes/create");
+    await router.isReady();
 
-    const nav = queryByRole('navigation');
+    const nav = queryByRole("navigation");
     expect(nav).toBeInTheDocument();
   });
 
-  it('renders an image when an icon is provided', () => {
-    const props = { ...initialProps, iconSrc: 'icon' };
+  it("renders an image when an icon is provided", async () => {
+    const props = { ...initialProps, iconSrc: "icon" };
     const { queryByRole } = renderComponent({ props });
+    router.push("/envelopes/create");
+    await router.isReady();
 
-    const img = queryByRole('img');
+    const img = queryByRole("img");
     expect(img).toBeInTheDocument();
   });
 
-  it('renders the correct number of breadcrumbs', () => {
+  it("renders the correct number of breadcrumbs", async () => {
     const props = initialProps;
 
-    const { queryAllByRole } = renderComponent({ props }, configureWithCurrentRoute);
+    const { queryAllByRole } = renderComponent({ props });
+    router.push("/envelopes/create");
+    await router.isReady();
 
-    const links = queryAllByRole('link');
+    const links = queryAllByRole("link");
     expect(links).toHaveLength(3);
   });
 
-  it('renders the last item as active', () => {
+  it("renders the last item as active", async () => {
     const props = initialProps;
 
-    const { queryAllByRole } = renderComponent({ props }, configureWithCurrentRoute);
+    const { queryAllByRole } = renderComponent({ props });
+    router.push("/envelopes/create");
+    await router.isReady();
 
-    const links = queryAllByRole('link');
-    expect(links[2]).toHaveClass('router-link-exact-active');
+    const links = queryAllByRole("link");
+    expect(links[2]).toHaveClass("router-link-active");
   });
-
 });
--- src/components/Subnavigation/__tests__/Subnavigation.spec.js
+++ src/components/Subnavigation/__tests__/Subnavigation.spec.js
@@ -1,71 +1,47 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { createRouter, createMemoryHistory } from "vue-router";
+import { render } from "@testing-library/vue";
+import Subnavigation from "../Subnavigation.vue";
 
-import { render } from '@testing-library/vue';
-import Subnavigation from '../Subnavigation.vue';
-import SubnaviationItem from '../SubnavigationItem.vue';
 
-const initialProps = {
-  title: 'Account',
-  to: '/account'
-};
 const routes = [
-  { path: '/account', component: { template: '<div>Account</div>' } },
-  { path: '/api-keys', component: { template: '<div>API Keys</div>' } },
-  { path: '/payment', component: { template: '<div>Payment</div>' } }
+  { path: "", component: { template: "<div></div>" } },
+  { path: "/", component: { template: "<div>/</div>" } },
+  { path: "/account", component: { template: "<div>Account</div>" } },
+  { path: "/api-keys", component: { template: "<div>API Keys</div>" } },
+  { path: "/payment", component: { template: "<div>Payment</div>" } },
 ];
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
 
-const renderSubnav = (options, configure = null) => render(Subnavigation, { ...options }, configure);
-const renderSubnavItem = (options, configure = null) => render(SubnaviationItem, { ...options, routes }, configure);
+const renderComponent = async (options) => {
+  const result = render(Subnavigation, { ...options, global: { plugins: [router] } });
+  await router.isReady()
+  return result;
+}
 
-describe('Subnavigation', () => {
-
+describe("Subnavigation", () => {
   let slots;
   let slotContent;
 
   beforeEach(() => {
-    slotContent = 'I\'m a child';
+    slotContent = "I'm a child";
     slots = { default: [`<li>${slotContent}</li>`] };
   });
 
-  it('renders correctly', () => {
-    const { queryByRole } = renderSubnav();
+  it("renders correctly", async () => {
+    const { queryByRole } = await renderComponent();
 
-    const subnav = queryByRole('navigation');
+    const subnav = queryByRole("navigation");
     expect(subnav).toBeInTheDocument();
   });
 
-  it('renders the slot content', () => {
-    const { queryByText } = renderSubnav({ slots });
+  it("renders the slot content", async () => {
+    const { getByText } = await renderComponent({ slots });
 
-    const slot = queryByText(slotContent);
+    const slot = getByText(new RegExp(slotContent));
     expect(slot).toBeInTheDocument();
   });
-
-});
-
-describe('SubnavigationItem', () => {
-
-  it('renders correctly', () => {
-    const props = initialProps;
-    const { queryByText, queryByRole } = renderSubnavItem({ props });
-
-    let item = queryByText(props.title);
-    expect(item).toBeInTheDocument();
-
-    item = queryByRole('link');
-    expect(item).toBeInTheDocument();
-  });
-
-  it('adds the correct classes when the item is active', () => {
-    const props = initialProps;
-
-    const configureWithInitialRoute = (_vue, _store, router) => {
-      router.push('/account');
-    };
-    const { queryByTestId } = renderSubnavItem({ props }, configureWithInitialRoute);
-
-    const navItem = queryByTestId('subnav-item');
-    expect(navItem).toHaveClass('lob-active-border');
-  });
-
 });
