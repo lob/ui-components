2876d737414ea9a501cb0d4cfb071e755e795e9a
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-08 09:52:17 -0500

    lint files in .storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,20 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-docs",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@storybook/addon-postcss",
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-postcss'
   ],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ["vue-style-loader", "css-loader", "sass-loader", "postcss-loader"],
-      include: path.resolve(__dirname, "../"),
+      use: ['vue-style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      include: path.resolve(__dirname, '../')
     });
     return config;
-  },
+  }
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,17 @@
-import { app } from "@storybook/vue3";
-import * as configs from "../src/config";
+import { app } from '@storybook/vue3';
+import * as configs from '../src/config';
 
-import "../src/assets/styles/main.scss";
+import '../src/assets/styles/main.scss';
 
 app.use(configs.constants);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
-  layout: "centered",
+  layout: 'centered'
 };
