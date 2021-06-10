cc6b0bac352b1e0b3753ef571d29d899b5214428
Juan Andres Friss de Kereki <83967528+juanfriss@users.noreply.github.com>
2021-06-10 16:58:18 -0300

    DANG-122: Top navbar/header (#20)
    * Initial commit of a generic navbar
    
    * Adds public folder for icons
    
    * Adds wip tests
    
    * Proposal for componetizing menu items
    
    * Creates top navbar menuitem
    
    * Adds top navbar menuitem story
    
    * menu item lint fix
    
    * update menu item to handle external and internal relative links correctly
    
    * move menu item into menu folder
    
    * add spec for menu item
    
    * rename TopNavbarMenu to MegaMenu and add docs
    
    * Tailwindifies the megamenu
    
    * clean up stories and docs
    
    * add mega menu specs
    
    * Updates documentation and topNavbar responsiveness
    
    * Typo
    
    * make megamenu stories work with vue 3
    
    * Merges two unnecessary divs into one
    
    * Updates tests for Vue3
    
    * Delets svgs, adds megaMenu responsiveness and fixes megaMenuItem font size
    
    * Adds accessibility props
    
    * Removes aria expanded
    
    * Simplifies classes and solves visibility bug for megamenu
    
    * Updates unit tests
    
    * Typo
    
    * Lint
    
    * Lint
    
    * Lint
    
    * Lint
    
    * Final cleaning
    
    * Lint
    
    Co-authored-by: Juan Friss de Kereki <juanfkt93@gmail.com>
    Co-authored-by: Mercedes Bernard <mercedesrbernard@gmail.com>
    Co-authored-by: Beth Qiang <beth.qiang@lob.com>

---
+++ src/components/TopNavbar/TopNavbar.stories.js
@@ -0,0 +1,54 @@
+import routeDecorator from '../../../.storybook/routeDecorator';
+import TopNavbar from './TopNavbar.vue';
+import mdx from './TopNavbar.mdx';
+import MegaMenu from '../MegaMenu/MegaMenu.vue';
+import MegaMenuItem from '../MegaMenu/MegaMenuItem.vue';
+
+export default {
+  title: 'Components/TopNavbar',
+  component: TopNavbar,
+  subcomponents: { MegaMenu, MegaMenuItem },
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  },
+  decorators: [
+    routeDecorator()
+  ]
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { TopNavbar, MegaMenu, MegaMenuItem },
+  setup: () => ({ args }),
+  template: `
+    <topNavbar v-bind="args">
+      <img
+        :src="$getConst('lobAssetsUrl') + '/dashboard/navbar/lob-logo.svg'"
+        width="95"
+        alt=""
+      >
+      <div class="md:pl-6">
+        <MegaMenu id="1" title="Some menu" navKey="" :mobileNavs="{}">
+          <MegaMenuItem to="/settings/main/account" :imageSource="$getConst('lobAssetsUrl') + '/dashboard/navbar/settings.svg'" small>
+            Some menu item
+          </MegaMenuItem>
+          <MegaMenuItem to="/settings/main/account" :imageSource="$getConst('lobAssetsUrl') + '/dashboard/navbar/settings.svg'" small>
+            Another menu item
+          </MegaMenuItem>
+        </MegaMenu>
+
+        <MegaMenu id="2" title="Another menu" navKey="" :mobileNavs="{}">
+          <MegaMenuItem to="/settings/main/account" :imageSource="$getConst('lobAssetsUrl')+'/dashboard/navbar/settings.svg'" small>
+            Yet another menu item
+          </MegaMenuItem>
+        </MegaMenu>
+      </div>
+    </topNavbar>
+  `
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
