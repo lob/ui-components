15f5a411002c1fc60dd6bb0d9952b8065305378f
Juan Friss de Kereki <juanfkt93@gmail.com>
2021-06-11 18:38:39 -0300

    Changes arrow directions

--- src/components/MainNavigation/MainNavigation.vue
+++ src/components/MainNavigation/MainNavigation.vue
@@ -9,7 +9,7 @@
     >
       <chevron-left
         v-if="collapsible"
-        :class="['w-6 h-6 pr-0.5 cursor-pointer absolute z-10 text-gray-300 bg-white bg-opacity-100 border-2 border-white-300 rounded-xl -right-3 mt-3', { 'transform rotate-180': expanded }]"
+        :class="['w-6 h-6 pr-0.5 cursor-pointer absolute z-10 text-gray-300 bg-white bg-opacity-100 border-2 border-white-300 rounded-xl -right-3 mt-3', { 'transform rotate-180': !expanded }]"
         role="button"
         :aria-expanded="expanded"
         @click="animateDrawer"
