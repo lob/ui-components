d5aa12ae703683e412fb749d9a9bf0f5a28f1096
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-14 12:04:11 -0500

    remove unused date utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,3 @@
-const ISO_DATE_FORMAT = /^(\d{4})-(\d{2})-(\d{2})$/;
-
 export const DaysOfWeek = {
   Sunday: 0,
   Monday: 1,
@@ -9,69 +7,6 @@ export const DaysOfWeek = {
   Friday: 5,
   Saturday: 6
 };
-
-export function createDate (year, month, day) {
-  var dayInt = parseInt(day, 10);
-  var monthInt = parseInt(month, 10);
-  var yearInt = parseInt(year, 10);
-
-  const isValid =
-    Number.isInteger(yearInt) && // all parts should be integers
-    Number.isInteger(monthInt) &&
-    Number.isInteger(dayInt) &&
-    monthInt > 0 && // month must be 1-12
-    monthInt <= 12 &&
-    dayInt > 0 && // day must be 1-31
-    dayInt <= 31 &&
-    yearInt > 0;
-
-  if (isValid) {
-    return new Date(yearInt, monthInt - 1, dayInt);
-  }
-}
-
-/**
- * @param {string} value date string in ISO format YYYY-MM-DD
- * @returns {Date}
- */
-export function parseISODate (value) {
-  if (!value) {
-    return;
-  }
-
-  const matches = value.match(ISO_DATE_FORMAT);
-
-  if (matches) {
-    return createDate(matches[1], matches[2], matches[3]);
-  }
-}
-
-/**
- * print date in format YYYY-MM-DD
- * @param {Date} date
- * @returns {string}
- */
-export function printISODate (date) {
-  if (!date) {
-    return '';
-  }
-
-  var d = date.getDate().toString(10);
-  var m = (date.getMonth() + 1).toString(10);
-  var y = date.getFullYear().toString(10);
-
-  // days are not zero-indexed, so pad if less than 10
-  if (date.getDate() < 10) {
-    d = `0${d}`;
-  }
-
-  // months *are* zero-indexed, pad if less than 9!
-  if (date.getMonth() < 9) {
-    m = `0${m}`;
-  }
-
-  return `${y}-${m}-${d}`;
-}
 
 export function addDays (date, days) {
   var d = new Date(date);
