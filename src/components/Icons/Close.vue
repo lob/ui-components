c71cf4a64415dc136acc4f75b110a96d4a25b4cd
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-14 10:57:27 -0500

    add close icon

---
+++ src/components/Icons/Close.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    viewBox="0 0 18 18"
+    class="fill-current stroke-current"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1 1L17 17"
+      stroke-width="2"
+    />
+    <path
+      d="M1 17L17 1"
+      stroke-width="2"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Close'
+};
+</script>
--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,5 +1,6 @@
 export { default as ChevronDown } from './ChevronDown';
 export { default as ChevronLeft } from './ChevronLeft';
 export { default as ChevronRight } from './ChevronRight';
+export { default as Close } from './Close';
 export { default as SkipToLeft } from './SkipToLeft';
 export { default as SkipToRight } from './SkipToRight';
