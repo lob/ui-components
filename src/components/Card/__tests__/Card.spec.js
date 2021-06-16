b3187aaa630c8ca4ad4cce29e8771fae9dd852d1
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-16 09:33:03 -0500

    add missing Card test

---
+++ src/components/Card/__tests__/Card.spec.js
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import Card from '../Card.vue';
+
+let slots;
+
+const renderComponent = (options) => render(Card, { ...options });
+
+describe('Card', () => {
+
+  it('renders correctly', () => {
+    const slotContent = 'Hello, this is a card.';
+    slots = { default: slotContent };
+    const { queryByText } = renderComponent({ slots });
+
+    const alertContent = queryByText(slotContent);
+    expect(alertContent).toBeInTheDocument();
+  });
+
+});
