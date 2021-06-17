9daa1989f729999dc2de9d2380c3a072096d7159
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-17 16:59:43 -0500

    add missing export for Search icon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -4,5 +4,6 @@ export { default as ChevronDown } from './ChevronDown';
 export { default as ChevronLeft } from './ChevronLeft';
 export { default as ChevronRight } from './ChevronRight';
 export { default as Close } from './Close';
+export { default as Search } from './Search';
 export { default as SkipToLeft } from './SkipToLeft';
 export { default as SkipToRight } from './SkipToRight';
