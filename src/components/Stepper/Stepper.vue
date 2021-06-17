9ed8e62fb49f95d236497427048712f2c9ad9138
Juan Andres Friss de Kereki <83967528+juanfriss@users.noreply.github.com>
2021-06-17 19:06:04 -0300

    Dang 149/Stepper (#38)

---
+++ src/components/Stepper/Stepper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="m-3">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stepper',
+  props: {
+  },
+  data () {
+    return {
+    };
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
