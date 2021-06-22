e68dcd9773ab38cf4d119d21c0c89f8ffc5044a7
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-06-22 11:40:06 -0700

    Header tweaks (#50)
    * Make header full-width.
    
    * Make mega menu item image optional
    
    Co-authored-by: Joshua Maros <joshua.maros@lob.com>

--- src/components/TopNavbar/TopNavbar.vue
+++ src/components/TopNavbar/TopNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="left-0 top-0 bg-white fixed w-full rounded">
     <nav
-      class="z-30 max-w-7xl w-full m-0 m-auto py-4 px-9 md:px-12 lg:px-6 md:flex mx-auto items-center bg-transparent w-auto max-w-none relative justify-between lg:justify-start"
+      class="z-30 w-full m-0 m-auto py-4 px-9 md:px-12 lg:px-6 md:flex mx-auto items-center bg-transparent w-auto max-w-none relative justify-between lg:justify-start"
     >
       <slot />
     </nav>
