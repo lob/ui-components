635968813c027132bb4552425fa935917d0ee439
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-06-24 14:18:54 -0500

    wip

--- src/components/TopNavbar/TopNavbar.vue
+++ src/components/TopNavbar/TopNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="left-0 top-0 bg-white fixed w-full rounded">
+  <div class="left-0 top-0 bg-white fixed w-full rounded border-b-2 border-white-300">
     <nav
       class="z-30 w-full m-0 m-auto py-4 px-9 md:px-12 lg:px-6 md:flex mx-auto items-center bg-transparent w-auto max-w-none relative justify-between lg:justify-start"
     >
