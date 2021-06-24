debea296b86e2e92d7a2f41078097d69009fc285
Mercedes <mercedesb@users.noreply.github.com>
2021-06-24 16:00:38 -0500

    vue-doc-gen was creating warnings because of missing default exports (#57)

---
+++ src/components/Typography/H2.vue
@@ -0,0 +1,11 @@
+<script>
+import Typography from './Typography';
+
+export default {
+  name: 'H2',
+  components: {
+    Typography
+  },
+  template: '<Typography variant="heading2"><slot /></Typography>'
+};
+</script>
---
+++ src/components/Typography/P.vue
@@ -0,0 +1,11 @@
+<script>
+import Typography from './Typography';
+
+export default {
+  name: 'P',
+  components: {
+    Typography
+  },
+  template: '<Typography variant="body1"><slot /></Typography>'
+};
+</script>
