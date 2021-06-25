3905b2a0720d33602d5c58dcf0e9f2c82904780c
Juan Andres Friss de Kereki <83967528+juanfriss@users.noreply.github.com>
2021-06-25 17:40:48 -0300

    Creates generic modal component (#54)
    * Creates generic modal component

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,7 @@ export { default as MainNavigationItem } from './MainNavigation/MainNavigationIt
 export { default as MainNavigationChildItem } from './MainNavigation/MainNavigationChildItem';
 export { default as MegaMenu } from './MegaMenu/MegaMenu';
 export { default as MegaMenuItem } from './MegaMenu/MegaMenuItem';
+export { default as Modal } from './Modal/Modal';
 export { default as Pagination } from './Pagination/Pagination';
 export { default as RadioButton } from './RadioButton/RadioButton';
 export { default as RadioGroup } from './RadioGroup/RadioGroup';
