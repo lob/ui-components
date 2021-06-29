f63608ccc5fb435d50f6e6cb8712fd74610c92fe
Joshua Maros <joshua.maros@lob.com>
2021-06-29 09:16:10 -0700

    Add margins on the bottom of headers.

--- src/components/Typography/Typography.vue
+++ src/components/Typography/Typography.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     :class="[
-      { 'text-5xl mt-16 font-bold': variant === 'heading1' },
-      { 'text-3xl mt-10 font-bold': variant === 'heading2' },
-      { 'text-lg mt-6 uppercase text-gray-500 font-medium': variant === 'heading3' },
+      { 'text-5xl mt-16 mb-2 font-bold': variant === 'heading1' },
+      { 'text-3xl mt-10 mb-2 font-bold': variant === 'heading2' },
+      { 'text-lg mt-6 mb-2 uppercase text-gray-500 font-medium': variant === 'heading3' },
       { 'my-2': variant === 'body1' },
       { 'text-sm my-2 text-gray-500': variant === 'caption' }
     ]"
