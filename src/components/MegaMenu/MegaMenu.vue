57e68eb30f0c4b56663bfa35fb2bedf5177b1f4f
Juan Friss de Kereki <juanfkt93@gmail.com>
2021-07-02 12:18:28 -0300

    Minor padding change for megamenu

--- src/components/MegaMenu/MegaMenu.vue
+++ src/components/MegaMenu/MegaMenu.vue
@@ -12,7 +12,7 @@
       :aria-controls="dropdownListId"
       aria-haspopup="menu"
     >
-      <div class="flex-nowrap flex mt-0 flex-row justify-between md:justify-start items-center p-1.5 md:p-0">
+      <div class="flex-nowrap flex mt-0 flex-row justify-between md:justify-start items-center px-2 py-1 md:px-0 md:py-0">
         {{ title }}
         <img
           :src="`${$getConst('lobAssetsUrl')}/dashboard/navbar/caret-down.svg`"
