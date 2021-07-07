9ee77afb90cfcbc5bebf125ac294657c583ea9ee
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-07-07 09:32:23 -0500

    cleanup story

--- src/components/Datepicker/Datepicker.stories.js
+++ src/components/Datepicker/Datepicker.stories.js
@@ -16,10 +16,45 @@ export default {
 };
 
 const dateModel = null;
-const dateModel2 = null;
 const show = false;
-const show2 = false;
 const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {  Datepicker },
+  data: () => ({ dateModel, show }),
+  setup: () => ({ args }),
+  template: `
+  <div class="relative">
+    <label>
+      Enter a date
+      <input @click.stop="show = !show" :value="dateModel" class="border border-gray-300">
+    </label>
+    <datepicker v-bind="args" v-model="dateModel" v-model:open="show"></datepicker>
+  </div>
+  `
+});
+
+const today = new Date();
+const oneYearAgo = new Date();
+const oneYearFromNow = new Date();
+oneYearAgo.setMonth(today.getMonth() - 12);
+oneYearFromNow.setMonth(today.getMonth() + 12);
+
+const isDateDisabled = (date) => {
+  const dayOfWeek = date.getDay();
+  return dayOfWeek === DaysOfWeek.Saturday || dayOfWeek === DaysOfWeek.Sunday;
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  id: 'test',
+  isDateDisabled,
+  min: oneYearAgo,
+  max: oneYearFromNow
+};
+
+const dateModel2 = null;
+const show2 = false;
+const WithMultipleTemplate = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: {  Datepicker },
   data: () => ({ dateModel, show, dateModel2, show2 }),
@@ -44,19 +79,8 @@ const Template = (args, { argTypes }) => ({
   `
 });
 
-const today = new Date();
-const oneYearAgo = new Date();
-const oneYearFromNow = new Date();
-oneYearAgo.setMonth(today.getMonth() - 12);
-oneYearFromNow.setMonth(today.getMonth() + 12);
-
-const isDateDisabled = (date) => {
-  const dayOfWeek = date.getDay();
-  return dayOfWeek === DaysOfWeek.Saturday || dayOfWeek === DaysOfWeek.Sunday;
-};
-
-export const Primary = Template.bind({});
-Primary.args = {
+export const WithMultipleDatepickers = WithMultipleTemplate.bind({});
+WithMultipleDatepickers.args = {
   id: 'test',
   isDateDisabled,
   min: oneYearAgo,
