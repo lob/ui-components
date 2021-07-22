193092958e1537df8783d700ba6a6f741b96afe8
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-07-22 11:16:14 -0500

    add additional test coverage and minor cleanup

---
+++ src/components/Stepper/__tests__/Stepper.spec.js
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import Stepper from '../Stepper.vue';
+
+let slots;
+
+const renderComponent = (options) => render(Stepper, { ...options });
+
+describe('Stepper', () => {
+
+  it('renders correctly', () => {
+    const slotContent = 'Stepper stuff';
+    slots = { default: slotContent };
+    const { queryByText } = renderComponent({ slots });
+
+    const stepperContent = queryByText(slotContent);
+    expect(stepperContent).toBeInTheDocument();
+  });
+
+});
