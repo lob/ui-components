b716a4d41c1da102e711e89f01252a91e927f532
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-08-07 09:20:57 -0500

    fix table components so props are passed through (and rename file)

--- src/components/Table/TableHeader.vue
+++ src/components/Table/TableHeader.vue
@@ -4,15 +4,7 @@ import { h } from 'vue';
 export default {
   name: 'TableHeader',
   render () {
-    const index = this.$slots
-      .default();
-    const children = [];
-    for (let i = 0; i < index.length; ++i) {
-      children.push(index[i].children);
-    }
-    const columns = children
-      .flat()
-      .map((slotItem) => h('td', [slotItem]));
+    const columns = this.$slots.default().map((slotItem) => h('td', slotItem));
     const tr = h('tr', columns);
     return h('thead', tr);
   }
--- src/components/Table/TableRow.vue
+++ src/components/Table/TableRow.vue
@@ -3,15 +3,7 @@ import { h } from 'vue';
 export default {
   name: 'TableRow',
   render () {
-    const index = this.$slots
-      .default();
-    const children = [];
-    for (let i = 0; i < index.length; ++i) {
-      children.push(index[i].children);
-    }
-    const rows = children
-      .flat()
-      .map((slotItem) => h('td', [slotItem]));
+    const rows = this.$slots.default().map((slotItem) => h('td', slotItem));
     return h('tr', rows);
   }
 };
