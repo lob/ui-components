31a0a81fba293eec37542a27baacddcea5ecf7d7
Anoop Kotha <46906679+akotha7@users.noreply.github.com>
2021-08-09 10:32:08 -0400

    Refactor table header for nesting (#130)
    * refactor table header for nesting
    
    * fix table components so props are passed through (and rename file)
    
    * make table header and row optionally handle a nested slot
    
    Co-authored-by: Mercedes Bernard <mercedesrbernard@gmail.com>

--- src/components/Table/TableRow.vue
+++ src/components/Table/TableRow.vue
@@ -3,15 +3,16 @@ import { h } from 'vue';
 export default {
   name: 'TableRow',
   render () {
-    const index = this.$slots
-      .default();
-    const children = [];
-    for (let i = 0; i < index.length; ++i) {
-      children.push(index[i].children);
+    const defaultSlot = this.$slots.default();
+    const isNestedSlot = Array.isArray(defaultSlot[0].children);
+
+    let rows;
+    if (isNestedSlot) {
+      rows = defaultSlot.flatMap((slotItem) => slotItem.children).map((child) => h('td', child));
+    } else {
+      rows = defaultSlot.map((slotItem) => h('td', slotItem));
     }
-    const rows = children
-      .flat()
-      .map((slotItem) => h('td', [slotItem]));
+
     return h('tr', rows);
   }
 };
