19c2ba66ea146716ecb7f0683090b7a7e49b47d4
akotha7 <akotha@umich.edu>
2021-08-09 08:42:23 -0400

    refactor table header for nesting

--- src/components/Table/TableHeader.vue
+++ src/components/Table/TableHeader.vue
@@ -4,11 +4,16 @@ import { h } from 'vue';
 export default {
   name: 'TableHeader',
   render () {
-    const columns = this.$slots
-      .default()
+    const index = this.$slots
+      .default();
+    const children = [];
+    for (let i = 0; i < index.length; ++i) {
+      children.push(index[i].children);
+    }
+    const columns = children
+      .flat()
       .map((slotItem) => h('td', [slotItem]));
     const tr = h('tr', columns);
-
     return h('thead', tr);
   }
 };
