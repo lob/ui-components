92b720e59f9271a784fc2546e39121561fc60e27
Mercedes <mercedesb@users.noreply.github.com>
2021-08-17 10:22:16 -0500

    add list and map icons to icon library (#139)

---
+++ src/components/Icons/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    viewBox="0 0 16 16"
+    class="fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 0H2.82353V3H0V0ZM3.76471 0V3H16V0H3.76471ZM0 9H2.82353V6H0V9ZM3.76471 9H16V6H3.76471V9ZM0 15H2.82353V12H0V15ZM3.76471 15H16V12H3.76471V15Z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'List'
+};
+</script>
