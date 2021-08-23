c9173d7401203c847b86ee9103020ad2328f8521
Mercedes <mercedesb@users.noreply.github.com>
2021-08-23 10:01:47 -0500

    add loading indicator (#143)
    * add loading indicator
    
    * fix height and width for deployed storybook

---
+++ src/components/LoadingIndicator/LoadingIndicator.stories.js
@@ -0,0 +1,47 @@
+import LoadingIndicator from './LoadingIndicator.vue';
+import mdx from './LoadingIndicator.mdx';
+
+export default {
+  title: 'Components/Loading Indicator',
+  component: LoadingIndicator,
+  decorators: [
+    // hard-coding height and width b/c PurgeCSS removes classes not used in our components
+    () => ({ template: '<div style="height: 12rem; width: 24rem;"><story /></div>' })
+  ],
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {  LoadingIndicator },
+  setup: () => ({ args }),
+  template: '<loading-indicator></loading-indicator>'
+});
+
+export const Primary = Template.bind({});
+
+const dataPresent = false;
+
+const AfterContentLoadedTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {  LoadingIndicator },
+  setup: () => ({ args }),
+  data: () => ({ dataPresent }),
+  mounted () {
+    this.updateData();
+  },
+  methods: {
+    updateData () {
+      setTimeout(() => {
+        this.dataPresent = true;
+      }, 3000);
+    }
+  },
+  template: '<loading-indicator><div v-if="dataPresent">Loading spinner disappeared after 3 seconds</div></loading-indicator>'
+});
+
+export const AfterContentLoaded = AfterContentLoadedTemplate.bind({});
