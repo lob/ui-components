268dd8610238128d342435f0e5b31f9302be6230
Shanna Murry <shanna.murry84@gmail.com>
2021-10-21 16:06:40 -0400

    Dang 450 badge hover fix (#174)
    * fixed active class not applying to editions tab
    
    * fixed active class in nav
    
    * fixed active class in nav
    
    * removed hover class from badge
    
    * removed hover class from badge
    
    Co-authored-by: Shanna Murry <shanna-murray@lob-mbp-sm-c02g719xmd6p.myfiosgateway.com>
    Co-authored-by: Shanna Murry <shanna-murray@proxy-nlb-prod-us-west-2-v5-ac4e52c97755301b.elb.us-west-2.amazonaws.com>

--- src/components/Badge/Badge.vue
+++ src/components/Badge/Badge.vue
@@ -4,12 +4,12 @@
       :class="['flex justify-center align-center',
                { 'px-4 py-2 rounded-full': rounded },
                { 'px-2 py-1 rounded-sm': square },
-               { 'bg-white-300 text-gray-500 hover:bg-gray-100': info },
-               { 'bg-mint-100 text-mint-900 hover:bg-mint-300': success },
-               { 'bg-lemon-100 text-lemon-900 hover:bg-lemon-300': warning },
-               { 'bg-coral-100 text-coral-900 hover:bg-coral-300': error },
-               { 'bg-turquoise-100 text-turquoise-900 hover:bg-turquoise-300': turquoise },
-               { 'bg-flint-100 text-gray-700 hover:bg-flint-300': flint },
+               { 'bg-white-300 text-gray-500': info },
+               { 'bg-mint-100 text-mint-900': success },
+               { 'bg-lemon-100 text-lemon-900': warning },
+               { 'bg-coral-100 text-coral-900': error },
+               { 'bg-turquoise-100 text-turquoise-900': turquoise },
+               { 'bg-flint-100 text-gray-700': flint },
                { '!py-0.5': small }
       ]"
     >
