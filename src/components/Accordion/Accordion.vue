8dbbc3afaeff2cdbc759c64d0c6cc65792f12fd2
Sana T <sanatauqir92@gmail.com>
2022-02-04 16:26:10 -0800

    accordion files

---
+++ src/components/Accordion/Accordion.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="cursor-pointer flex justify-between min-w-full mb-6 t"
+    @click="expanded = !expanded"
+    :class="expanded ? null : 'border-b-2'"
+  >
+    <div class="text-primary-500 font-thin text-lg">
+      <b>{{ title }}</b>
+    </div>
+    <div class="flex-end">
+      <ChevronDown
+        v-if="expanded"
+        class="w-6 h-6"
+      />
+      <ChevronRight
+        v-else
+        class="w-6 h-6"
+      />
+    </div>
+  </div>
+  <div
+    v-if="expanded"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+import ChevronRight from '../Icons/ChevronRight';
+import ChevronDown from '../Icons/ChevronDown';
+
+export default {
+  name: 'Accordion',
+  components: { ChevronRight, ChevronDown },
+  props: {
+    title: {
+      type: String,
+      default: 'Accordion Title'
+    }
+  },
+  data () {
+    return {
+      expanded: false
+    };
+  }
+
+};
+</script>
