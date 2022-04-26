cedab039b279c7080be666cad3f6f69bfb8835f9
Myrtle <myrtle.tzouma@lob.com>
2022-04-26 12:42:41 -0400

    rebase & upgrade tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,5 @@ module.exports = {
   content: ['./src/**/*.vue'],
   plugins: [
     require('tailwind-plugin-lob')
-  ],
-  variants: {
-    extend: {
-      width: ['hover'],
-      padding: ['hover']
-    }
-  }
+  ]
 };
