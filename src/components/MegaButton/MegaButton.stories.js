8cac0a1be55c29e1f2b1c03977be7ceb65a060a5
Liss McCabe <liss@eristiccode.com>
2022-04-28 22:40:16 -0700

    wip need to do strikethru, images, group heights and widths

--- src/components/MegaButton/MegaButton.stories.js
+++ src/components/MegaButton/MegaButton.stories.js
@@ -8,32 +8,6 @@ export default {
     docs: {
       page: mdx
     }
-  },
-  argTypes: {
-    content: {
-      control: {
-        type: 'text'
-      },
-      description: 'Content to display inside of the button',
-      table: {
-        defaultValue: 'Sign up',
-        type: {
-          summary: 'html or component'
-        }
-      }
-    },
-    variant: {
-      options: ['primary', 'none'],
-      control: {
-        type: 'select'
-      }
-    },
-    size: {
-      options: ['default', 'small', 'large'],
-      control: {
-        type: 'select'
-      }
-    }
   }
 };
 
@@ -41,11 +15,51 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { MegaButton },
   setup: () => ({ args }),
-  template: '<mega-button v-bind="args" v-slot>{{ args.content }}</lob-button>'
+  template: '<MegaButton v-bind="args"></MegaButton>'
 });
 
 export const Primary = Template.bind({});
 Primary.args = {
-  title: 'Cat dispenser',
-  text: 'Click here to receive catte'
+  name: 'cat-type',
+  id: 'calico',
+  label: 'Calico',
+  value: 'calico',
+  disabled: false,
+  disabledBanner: null,
+  text: null
 };
+
+export const WithText = Template.bind({});
+WithText.args = {
+  name: 'cat-type',
+  id: 'ginger',
+  value: 'ginger',
+  label: 'Ginger',
+  disabled: false,
+  disabledBanner: null,
+  text: 'The himbos of the cat world'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  name: 'cat-type',
+  id: 'void',
+  value: 'void',
+  label: 'Voidcat',
+  disabled: true,
+  disabledBanner: 'Hiding in closet!',
+  text: 'Like a loveable little black hole that sheds on your white jeans'
+};
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+  name: 'cat-type',
+  id: 'floof',
+  value: 'floof',
+  label: 'Floofcat',
+  disabled: false,
+  disabledBanner: null,
+  text: 'Probably a pillow',
+  imageSource: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Cymric_-_Norwegian_forest_cat_presentation_show_Kotka_2009-02-01_IMG_0687.JPG'
+};
+
--- src/components/MegaButton/MegaButton.vue
+++ src/components/MegaButton/MegaButton.vue
@@ -1,74 +1,142 @@
 <template>
-  <button
-    :class="[
-      'flex justify-center items-center disabled:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent',
-      { 'rounded': !link || !none },
-      { 'cursor-not-allowed': disabled },
-      { 'border-none': disabled && primary },
-      {
-        'primary bg-primary-500 border border-primary-500 text-white active:bg-primary-700 disabled:bg-white-300': primary
-      },
-      { 'block': none },
-      {
-        'bg-opacity-50': (success || error) && disabled
-      },
-      { 'px-6 py-3.5': defaultSize },
-      { 'px-3 py-2': small },
-      { 'px-6 py-4.5': large },
-      { '!p-0': link || none }
-    ]"
+  <input
+    :id="id"
+    type="radio"
+    :name="name"
+    :value="value.toString()"
+    :checked="checked"
     :disabled="disabled"
-    @click="handleClick"
+    :readonly="readonly"
+    :required="required"
+    @input="onInput"
+    @click="onClick"
   >
-    <span class="flex justify-between items-center">
-      <slot />
-    </span>
-  </button>
+  <label
+    :for="id"
+    :class="[
+      'megabutton flex flex-col rounded-lg m-5 inline-block py-9 px-8 border-4 border-transparent relative'
+    ]"
+  >
+    <div
+      v-if="disabled && disabledBanner"
+      class="absolute mt-0 text-center bg-turquoise-100 uppercase text-gray-700 font-bold p-1 inset-x-0 top-0"
+    >
+      {{ disabledBanner }}
+    </div>
+
+    <div>
+
+      <div
+        v-if="textWithImage"
+        class="w-28"
+      >
+        <img
+          :src="
+            imageSource"
+        >
+      </div>
+
+      <!-- TODO: Make height and width consistent across a group -->
+      <div :class="['font-semibold', { 'text-lg w-36': withText }, { 'text-center text-xl': largeText }]">
+        <slot>
+          {{ label }}
+        </slot>
+      </div>
+      <div
+        v-if="withText"
+        class="text-left"
+      >
+        <p>{{ text }}</p>
+      </div>
+    </div></label>
 </template>
 
 <script>
+import { getCurrentInstance } from 'vue';
+
 export default {
-  name: 'MegaButton',
+  name: 'RadioButton',
   props: {
-    variant: {
+    id: {
       type: String,
-      default: 'primary',
-      validator: function (value) {
-        return ['primary', 'none'].includes(value);
-      }
+      required: true
     },
-    size: {
+    modelValue: {
+      type: [String, Boolean],
+      default: null
+    },
+    name: {
       type: String,
-      default: 'default',
-      validator: function (value) {
-        return ['default', 'small', 'large'].includes(value);
-      }
+      default: ''
+    },
+    value: {
+      type: [String, Boolean],
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    disabledBanner: {
+      type: String,
+      default: null
+    },
+    imageSource: {
+      type: String,
+      default: null
+    },
+    required: {
+      type: Boolean,
+      default: false
     },
     disabled: {
       type: Boolean,
       default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
     }
   },
-  emits: ['click'],
+  emits: ['update:modelValue', 'input', 'click'],
+  data () {
+    return {
+      parent: null
+    };
+  },
   computed: {
-    primary () {
-      return this.variant === 'primary';
+    checked () {
+      return this.modelValue === this.value;
     },
-    none () {
-      return this.variant === 'none';
+    textWithoutImage () {
+      return !this.imageSource && this.label && this.text;
     },
-    defaultSize () {
-      return this.size === 'default';
+    textWithImage () {
+      return this.imageSource && this.label && this.text;
     },
-    small () {
-      return this.size === 'small';
+    largeText () {
+      return !this.imageSource && this.label && !this.text;
     },
-    large () {
-      return this.size === 'large';
+    withText () {
+      return this.label && this.text;
+    },
+    withDisabledBanner () {
+      return this.disabled && this.disabledBanner;
     }
+  },
+  created () {
+    this.parent = getCurrentInstance().parent;
   },
   methods: {
-    handleClick ($event) {
+    onInput () {
+      this.$emit('update:modelValue', this.value);
+      this.$emit('input', this.value);
+    },
+    onClick ($event) {
       this.$emit('click', $event);
     }
   }
@@ -76,7 +144,44 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.primary:hover:not(:disabled):not(:focus) {
-  box-shadow: 0 0 10px 2px rgba(24, 118, 219, 0.6);
+primary-cobalt {
+    color: #1876DB;
+}
+
+input[type='radio'] {
+    @apply hidden;
+
+    &:not(:disabled) ~ label {
+        @apply cursor-pointer;
+    }
+}
+
+/* TODO: Make strike line show only when there's no disabled banner */
+input[type="radio"]:disabled + label {
+    cursor: not-allowed;
+    border: 1px solid rgba(196, 196, 196, 1);
+    box-shadow: none;
+    color: #8c8c8c;
+    background: linear-gradient(to top right, #fff calc(50% - 1px), #C4C4C4, #fff calc(50% + 1px) );
+}
+
+input[type="radio"]:hover + label {
+    box-shadow: 2px 4px 16px 1px rgba(0, 0, 0, 0.06), 0px 44.8255px 29.6538px rgba(0, 0, 0, 0.0425185), 0px 23.2748px 15.1286px rgba(0, 0, 0, 0.035), 0px 9.48231px 7.58585px rgba(0, 0, 0, 0.0274815), 0px 2.15507px 3.66362px rgba(0, 0, 0, 0.0168519)
+}
+
+/* TODO: get behavior right with focused and selected simultaneously */
+input[type="radio"]:focus + label {
+    border: 4px solid #7CC6DD;
+}
+
+input[type="radio"]:checked + label {
+    box-sizing: border-box;
+    border: 2px solid rgba(24, 118, 219, 1);
+
+    box-shadow: 2px 4px 16px 1px rgba(0, 0, 0, 0.06), 0px 44.8255px 29.6538px rgba(0, 0, 0, 0.0425185), 0px 23.2748px 15.1286px rgba(0, 0, 0, 0.035), 0px 9.48231px 7.58585px rgba(0, 0, 0, 0.0274815), 0px 2.15507px 3.66362px rgba(0, 0, 0, 0.0168519)
+}
+
+label {
+    box-shadow: 0px 4.32px 12.95px rgba(0, 0, 0, 0.08);
 }
 </style>
