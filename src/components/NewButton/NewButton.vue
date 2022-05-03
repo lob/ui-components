3b2e9e2fc9f452a4b7e9825c9dd16b2bc5ee0500
Myrtle <myrtle.tzouma@lob.com>
2022-05-03 13:02:19 -0700

    newbutton updates: fixed height, letter-spacing, focus-visible, small size

--- src/components/NewButton/NewButton.vue
+++ src/components/NewButton/NewButton.vue
@@ -1,13 +1,15 @@
 <template>
   <button
     :class="[
-      'flex justify-center items-center rounded-lg focus:ring-4 focus:outline-none',
-      { 'px-8 py-2.5 font-bold textTwenty': primary || secondary },
-      { 'p-0 text-primary-500 underline disabled:text-gray-500': link },
-      { 'p-0 disabled:text-gray-500': none },
+      'flex justify-center items-center rounded-lg tracking-[-0.04em] focus-visible:ring-4 focus-visible:outline-none',
+      { 'font-bold': primary || secondary },
+      { 'px-8 text-[20px] h-[48px]': regular },
+      { 'px-6 text-[14px] h-[32px]': small },
+      { 'p-0 text-base text-primary-500 underline disabled:text-gray-500': link },
+      { 'p-0 text-base disabled:text-gray-500': none },
       { 'cursor-not-allowed': disabled },
-      { 'focus:ring-primary-100': !warning },
-      { 'focus:ring-coral-700': warning },
+      { 'focus-visible:ring-primary-100': !warning },
+      { 'focus-visible:ring-coral-700': warning },
       { 'primary  bg-primary-500 text-white active:bg-black disabled:bg-gray-100': primary && !warning },
       { 'primary warning bg-coral-900 text-white active:bg-coral-700 disabled:bg-coral-200': primary && warning },
       { 'secondary border border-primary-500 bg-white text-primary-500': secondary && !warning,
@@ -35,6 +37,10 @@ export default {
         return ['primary', 'secondary', 'link', 'none'].includes(value);
       }
     },
+    small: {
+      type: Boolean,
+      default: false
+    },
     disabled: {
       type: Boolean,
       default: false
@@ -57,6 +63,9 @@ export default {
     },
     none () {
       return this.variant === 'none';
+    },
+    regular () {
+      return !this.small;
     }
   },
   methods: {
@@ -72,26 +81,26 @@ export default {
   font-size: 20px;
 }
 
-.primary:focus:not(:active):not(:disabled),
-.primary:hover:not(:disabled):not(:focus) {
+.primary:focus-visible:not(:active):not(:disabled),
+.primary:hover:not(:disabled):not(:focus-visible) {
   background: linear-gradient(105.01deg, #0154ac 17.25%, #1876db 93.21%);
 }
 
-.primary.warning:focus:not(:active):not(:disabled),
-.primary.warning:hover:not(:disabled):not(:focus) {
+.primary.warning:focus-visible:not(:active):not(:disabled),
+.primary.warning:hover:not(:disabled):not(:focus-visible) {
   background: linear-gradient(95.27deg, #611d18 4.76%, #943832 81.28%);
 }
 
-.secondary:hover:not(:disabled):not(:focus),
-.primary:hover:not(:disabled):not(:focus) {
+.secondary:hover:not(:disabled):not(:focus-visible),
+.primary:hover:not(:disabled):not(:focus-visible) {
   box-shadow:
     0 9px 12px 0 rgba(0, 0, 0, 0.2),
     0 19px 29px 0 rgba(0, 0, 0, 0.14),
     0 7px 36px 0 rgba(0, 0, 0, 0.12);
 }
 
-.primary:not(:disabled):focus:active,
-.secondary:not(:disabled):focus:active {
+.primary:not(:disabled):focus-visible:active,
+.secondary:not(:disabled):focus-visible:active {
   box-shadow: none;
 }
 </style>
