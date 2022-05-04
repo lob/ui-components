bfe90cbf7d9b8f7df09ee2f1818e76aabd20640a
Myrtle <myrtle.tzouma@lob.com>
2022-05-04 09:27:50 -0700

    fix active state bg, remove old class, remove heights from link & none variants

--- src/components/NewButton/NewButton.vue
+++ src/components/NewButton/NewButton.vue
@@ -1,12 +1,13 @@
 <template>
   <button
     :class="[
-      'flex justify-center items-center rounded-lg tracking-[-0.04em] focus-visible:ring-4 focus-visible:outline-none',
+      'flex justify-center items-center rounded-lg tracking-[-0.04em] focus-visible:ring-4 focus:outline-none',
       { 'font-bold': primary || secondary },
       { 'px-8 text-[20px] h-[48px]': regular },
       { 'px-6 text-[14px] h-[32px]': small },
-      { 'p-0 text-base text-primary-500 underline disabled:text-gray-500': link },
-      { 'p-0 text-base disabled:text-gray-500': none },
+      { 'px-0 h-full text-base': link || none },
+      { 'text-primary-500 underline disabled:text-gray-500': link },
+      { 'disabled:text-gray-500': none },
       { 'cursor-not-allowed': disabled },
       { 'focus-visible:ring-primary-100': !warning },
       { 'focus-visible:ring-coral-700': warning },
@@ -77,30 +78,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.textTwenty {
-  font-size: 20px;
-}
-
-.primary:focus-visible:not(:active):not(:disabled),
-.primary:hover:not(:disabled):not(:focus-visible) {
+.primary:focus:not(:active):not(:disabled),
+.primary:hover:not(:disabled):not(:focus) {
   background: linear-gradient(105.01deg, #0154ac 17.25%, #1876db 93.21%);
 }
 
-.primary.warning:focus-visible:not(:active):not(:disabled),
-.primary.warning:hover:not(:disabled):not(:focus-visible) {
+.primary.warning:focus:not(:active):not(:disabled),
+.primary.warning:hover:not(:disabled):not(:focus) {
   background: linear-gradient(95.27deg, #611d18 4.76%, #943832 81.28%);
 }
 
-.secondary:hover:not(:disabled):not(:focus-visible),
-.primary:hover:not(:disabled):not(:focus-visible) {
+.secondary:hover:not(:disabled):not(:focus),
+.primary:hover:not(:disabled):not(:focus) {
   box-shadow:
     0 9px 12px 0 rgba(0, 0, 0, 0.2),
     0 19px 29px 0 rgba(0, 0, 0, 0.14),
     0 7px 36px 0 rgba(0, 0, 0, 0.12);
 }
 
-.primary:not(:disabled):focus-visible:active,
-.secondary:not(:disabled):focus-visible:active {
+.primary:not(:disabled):focus:active,
+.secondary:not(:disabled):focus:active {
   box-shadow: none;
 }
 </style>
