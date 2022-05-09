da4bbc7341363e6930228f2f873c0c7defb3acef
Liss McCabe <liss@eristiccode.com>
2022-05-09 16:16:46 -0700

    update tests

--- src/components/MegaButton/__tests__/MegaButton.spec.js
+++ src/components/MegaButton/__tests__/MegaButton.spec.js
@@ -3,98 +3,124 @@ import { render, fireEvent } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
 import MegaButton from '../MegaButton.vue';
 
-const initialProps = {
-  id: 'megabutton',
-  modelValue: '',
-  name: 'Chonky Boi',
-  label: 'Megabutton',
-  value: 'bigOlButton',
-  disabled: false
-};
+let label;
+let props;
 
 describe('Megabutton', () => {
 
-  it('disables the input when disabled prop is true', () => {
-    const props = {
-      ...initialProps,
-      disabled: true
+  describe('Megatext format', () => {
+
+    const megatextProps = {
+      id: 'megabutton',
+      modelValue: '',
+      name: 'Chonky Boi',
+      label: 'Megabutton',
+      value: 'bigOlButton',
+      disabled: false
     };
 
-    const { getByLabelText } = render(MegaButton, {
-      props
+    it('disables the input when disabled prop is true', () => {
+      props = {
+        ...megatextProps,
+        disabled: true
+      };
+
+      const { getByLabelText } = render(MegaButton, {
+        props
+      });
+
+      const mega = getByLabelText(props.label);
+      expect(mega).toBeDisabled();
     });
 
-    const mega = getByLabelText(props.label);
-    expect(mega).toBeDisabled();
-  });
+    it('checks the input when the value matches the modelValue', () => {
+      const props = {
+        ...megatextProps,
+        modelValue: megatextProps.value
+      };
+      const { getByLabelText } = render(MegaButton, {
+        props
+      });
 
-  it('checks the input when the value matches the modelValue', () => {
-    const props = {
-      ...initialProps,
-      modelValue: initialProps.value
-    };
-    const { getByLabelText } = render(MegaButton, {
-      props
+      const mega = getByLabelText(props.label);
+      expect(mega).toBeChecked();
     });
 
-    const mega = getByLabelText(props.label);
-    expect(mega).toBeChecked();
-  });
+    it('fires the input and click events when the input is clicked', async () => {
+      props = megatextProps;
+      const { getByLabelText, emitted } = render(MegaButton, {
+        props
+      });
+      const mega = getByLabelText(props.label);
 
-  it('fires the input and click events when the input is clicked', async () => {
-    const props = initialProps;
-    const { getByLabelText, emitted } = render(MegaButton, {
-      props
-    });
-    const mega = getByLabelText(props.label);
-
-    await fireEvent.click(mega);
-    const emittedEvent = emitted();
-    expect(emittedEvent).toHaveProperty('click');
-    expect(emittedEvent).toHaveProperty('input');
-    expect(emittedEvent.input[0]).toEqual([props.value]);
-  });
-
-  it('displays the strikethru line when disabled without banner', () => {
-    const props = {
-      ...initialProps,
-      disabled: true
-    };
-
-    const { getByText } = render(MegaButton, {
-      props
+      await fireEvent.click(mega);
+      const emittedEvent = emitted();
+      expect(emittedEvent).toHaveProperty('click');
+      expect(emittedEvent).toHaveProperty('input');
+      expect(emittedEvent.input[0]).toEqual([props.value]);
     });
 
-    const label = getByText(props.label).closest('label');
-    expect(label).toHaveClass('strikethru-line');
-    expect(label).toHaveClass('cursor-not-allowed');
-  });
+    describe('when disabled', () => {
 
-  it('does not display the strikethru line when disabled with banner', () => {
-    const props = {
-      ...initialProps,
-      disabled: true,
-      disabledBanner: 'banner!'
-    };
+      describe('without disabled banner', () => {
 
-    const { getByText } = render(MegaButton, {
-      props
+        beforeAll(() => {
+
+          props = {
+            ...megatextProps,
+            disabled: true
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          label = getByText(props.label).closest('label');
+
+        });
+
+        it('has faded text and not allowed cursor', () => {
+          expect(label).toHaveClass('peer-disabled:text-gray-100');
+          expect(label).toHaveClass('peer-disabled:cursor-not-allowed');
+        });
+
+        it('displays the strikethru line', () => {
+          expect(label).toHaveClass('strikethru-line');
+        });
+
+      });
+
+      describe('with disabled banner', () => {
+
+        beforeAll(() => {
+
+          props = {
+            ...megatextProps,
+            disabled: true,
+            disabledBanner: 'Bruce'
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          label = getByText(props.label).closest('label');
+
+        });
+
+        it('has faded text and not allowed cursor', () => {
+          expect(label).toHaveClass('peer-disabled:text-gray-100');
+          expect(label).toHaveClass('peer-disabled:cursor-not-allowed');
+        });
+
+        it('does not display the strikethru line', () => {
+          expect(label).not.toHaveClass('strikethru-line');
+        });
+
+      });
+
     });
 
-    const label = getByText(props.label).closest('label');
-    expect(label).not.toHaveClass('strikethru-line');
-  });
-
-  it('shows the focus ring when focused', async () => {
-    const props = initialProps;
-
-    const { getByText } = render(MegaButton, {
-      props
-    });
-
-    const label = getByText(props.label).closest('label');
-    await userEvent.click(label);
-    expect(label).toHaveClass('border');
   });
 
 });
