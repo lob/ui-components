64945e277ccedc0b0849ecf85a7ae889af607190
Liss McCabe <liss@eristiccode.com>
2022-05-09 16:26:37 -0700

    remove unused import in test

--- src/components/MegaButton/__tests__/MegaButton.spec.js
+++ src/components/MegaButton/__tests__/MegaButton.spec.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/vue';
-import userEvent from '@testing-library/user-event';
 import MegaButton from '../MegaButton.vue';
 
 let label;
