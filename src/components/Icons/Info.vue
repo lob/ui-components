4d0bf6bd0d063dd478130847aa224a175b847ba1
Myrtle <50080618+Mrtly@users.noreply.github.com>
2022-05-10 11:17:09 -0700

    fix: give Icons currentColor instead of fixed color (#247)
    * give Icons currentColor instead of fixed color
    
    * v191
    
    * add text size 14 to the Alert

--- src/components/Icons/Info.vue
+++ src/components/Icons/Info.vue
@@ -1,7 +1,5 @@
 <template>
   <svg
-    width="24"
-    height="24"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -10,11 +8,11 @@
       cx="12"
       cy="12"
       r="8.5"
-      stroke="#57A1B9"
+      stroke="currentColor"
     />
     <path
       d="M12.302 7C11.574 7 11 7.546 11 8.274C11 9.002 11.574 9.548 12.302 9.548C13.03 9.548 13.618 9.002 13.618 8.274C13.618 7.546 13.03 7 12.302 7ZM13.45 17.416V11.276L11.168 12.276V17.416H13.45Z"
-      fill="#57A1B9"
+      fill="currentColor"
     />
   </svg>
 </template>
