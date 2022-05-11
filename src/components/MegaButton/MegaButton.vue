9084408b7e4da83680fe4cceac99ed5a06aa8605
Liss McCabe <liss@eristiccode.com>
2022-05-11 09:43:30 -0700

    Megabuttons, part 1: Megatext (#245)
    * wip
    
    * wip need to do strikethru, images, group heights and widths
    
    * bump tailwind version
    
    * wip: all the things, but I only want the megatext
    
    * Simplify for PR
    
    * here is documentation now
    
    * remove free package-lock
    
    * Revert "remove free package-lock"
    
    This reverts commit abf5c3fa2fac6b2bf7ffe69dbe3b029ce12edbfa.
    
    * linter fixes
    
    * there, happy now, linter?
    
    * update tests
    
    * remove unused import in test
    
    * remove unneeded parent bits
    
    * simplify test setup
    
    * use userEvent for clicks
    
    * remove package-lock changes
    
    * remove package-lock changes
    
    * oops
    
    * absolutely position input within label
    
    * fieldset! it wanted a fieldset
    
    * remove unused props
    
    * stop that horrible 2 px wiggle
    
    * oh, that text prop isn't supposed to exist yet
    
    * :facepalm:

---
+++ src/components/MegaButton/MegaButton.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <input
+      :id="id"
+      type="radio"
+      class="absolute top-1/2 left-1/2 opacity-0 peer"
+      :name="name"
+      :value="value.toString()"
+      :checked="checked"
+      :disabled="disabled"
+      @input="onInput"
+      @click="onClick"
+    >
+    <label
+      :for="id"
+      :class="[
+        'h-full',
+        'flex items-center rounded-lg m-5 inline-block border-2 border-transparent ring-4 ring-transparent relative shadow-input cursor-pointer',
+        'peer-focus:ring-primary-100 peer-focus:ring-4',
+        'peer-hover:shadow',
+        'peer-checked:border-2 peer-checked:border-primary-500',
+        'peer-disabled:cursor-not-allowed peer-disabled:shadow-none peer-disabled:text-gray-100',
+        'peer-disabled:border-2 peer-checked:border-disabled-gray',
+        { 'strikethru-line': disabled && !disabledBanner }
+      ]"
+    >
+      <div
+        v-if="hasDisabledBanner"
+        class="absolute mt-0  text-center bg-turquoise-100 uppercase text-gray-700 text-sm tracking-wide font-bold p-1 inset-x-0 top-0"
+      >
+        {{ disabledBanner }}
+      </div>
+
+      <div>
+        <div :class="['mx-8', { 'mt-8 mb-9': !hasDisabledBanner }, {'mt-12 mb-6': hasDisabledBanner }]">
+          <div class="font-semibold, text-center text-3xl">
+            <slot name="label">
+              {{ label }}
+            </slot>
+          </div>
+        </div>
+      </div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MegaButton',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: [String, Boolean],
+      default: null
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: [String, Boolean],
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    disabledBanner: {
+      type: String,
+      default: null
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['update:modelValue', 'input', 'click'],
+  computed: {
+    checked () {
+      return this.modelValue === this.value;
+    },
+    hasDisabledBanner () {
+      return this.disabled && this.disabledBanner;
+    }
+  },
+  methods: {
+    onInput () {
+      this.$emit('update:modelValue', this.value);
+      this.$emit('input', this.value);
+    },
+    onClick ($event) {
+      this.$emit('click', $event);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.strikethru-line {
+  background: linear-gradient(to top right, #fff calc(50% - 1px), #c4c4c4, #fff calc(50% + 1px));
+}
+
+label {
+  box-shadow: 0 4.32px 12.95px rgba(0, 0, 0, 0.08);
+}
+
+input[type="radio"]:disabled + label {
+  border: 1px solid #c4c4c4;
+  color: #8c8c8c;
+  box-shadow: none;
+}
+
+</style>
---
+++ src/components/MegaButton/__tests__/MegaButton.spec.js
@@ -0,0 +1,134 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+import MegaButton from '../MegaButton.vue';
+
+describe('Megabutton', () => {
+
+  describe('Megatext format', () => {
+
+    const megatextProps = {
+      id: 'megabutton',
+      modelValue: '',
+      name: 'Chonky Boi',
+      label: 'Megabutton',
+      value: 'bigOlButton',
+      disabled: false
+    };
+
+    it('disables the input when disabled prop is true', () => {
+      const props = {
+        ...megatextProps,
+        disabled: true
+      };
+
+      const { getByLabelText } = render(MegaButton, {
+        props
+      });
+
+      const mega = getByLabelText(props.label);
+      expect(mega).toBeDisabled();
+    });
+
+    it('checks the input when the value matches the modelValue', () => {
+      const props = {
+        ...megatextProps,
+        modelValue: megatextProps.value
+      };
+      const { getByLabelText } = render(MegaButton, {
+        props
+      });
+
+      const mega = getByLabelText(props.label);
+      expect(mega).toBeChecked();
+    });
+
+    it('fires the input and click events when the input is clicked', async () => {
+      const props = megatextProps;
+      const { getByLabelText, emitted } = render(MegaButton, {
+        props
+      });
+      const mega = getByLabelText(props.label);
+
+      await userEvent.click(mega);
+      const emittedEvent = emitted();
+      expect(emittedEvent).toHaveProperty('click');
+      expect(emittedEvent).toHaveProperty('input');
+      expect(emittedEvent.input[0]).toEqual([props.value]);
+    });
+
+    describe('when disabled', () => {
+
+      describe('without disabled banner', () => {
+
+        it('has faded text and not allowed cursor', () => {
+          const props = {
+            ...megatextProps,
+            disabled: true
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          const label = getByText(props.label).closest('label');
+          expect(label).toHaveClass('peer-disabled:text-gray-100');
+          expect(label).toHaveClass('peer-disabled:cursor-not-allowed');
+        });
+
+        it('displays the strikethru line', () => {
+          const props = {
+            ...megatextProps,
+            disabled: true
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          const label = getByText(props.label).closest('label');
+          expect(label).toHaveClass('strikethru-line');
+        });
+
+      });
+
+      describe('with disabled banner', () => {
+
+        it('has faded text and not allowed cursor', () => {
+          const props = {
+            ...megatextProps,
+            disabled: true,
+            disabledBanner: 'Bruce'
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          const label = getByText(props.label).closest('label');
+          expect(label).toHaveClass('peer-disabled:text-gray-100');
+          expect(label).toHaveClass('peer-disabled:cursor-not-allowed');
+        });
+
+        it('does not display the strikethru line', () => {
+          const props = {
+            ...megatextProps,
+            disabled: true,
+            disabledBanner: 'Bruce'
+          };
+
+          const { getByText } = render(MegaButton, {
+            props
+          });
+
+          const label = getByText(props.label).closest('label');
+          expect(label).not.toHaveClass('strikethru-line');
+        });
+
+      });
+
+    });
+
+  });
+
+});
