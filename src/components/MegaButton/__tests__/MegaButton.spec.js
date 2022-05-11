afceda7adc2b3569196c07e90acb0a274cc79f35
Liss McCabe <liss@eristiccode.com>
2022-05-11 13:42:33 -0700

    add tests for disabled banner in image version

--- src/components/MegaButton/__tests__/MegaButton.spec.js
+++ src/components/MegaButton/__tests__/MegaButton.spec.js
@@ -200,4 +200,93 @@ describe('Megabutton', () => {
 
   });
 
+  describe('Small text with image', () => {
+
+    const textWithImageProps = {
+      id: 'megabutton',
+      modelValue: '',
+      name: 'Fine print',
+      label: 'Megabutton with text',
+      text: 'This also has an image',
+      imageSource: 'fake_image.egg',
+      value: 'bigOlButton'
+    };
+
+    it('renders the label in smaller text', () => {
+      const props = textWithImageProps;
+
+      const { getByText } = render(MegaButton, {
+        props
+      });
+
+      const label = getByText(props.label);
+      expect(label).toHaveClass('text-lg');
+    });
+
+    it('renders the text', () => {
+      const props = textWithImageProps;
+
+      const { getByText } = render(MegaButton, {
+        props
+      });
+
+      const text = getByText(props.text);
+      expect(text).toBeValid();
+    });
+
+    describe('with disabled banner', () => {
+
+      it('has faded text and not allowed cursor', () => {
+        const props = {
+          ...textWithImageProps,
+          disabled: true,
+          disabledBanner: 'Bruce'
+        };
+
+        const { getByText } = render(MegaButton, {
+          props
+        });
+
+        const label = getByText(props.label).closest('label');
+        expect(label).toHaveClass('peer-disabled:text-gray-100');
+        expect(label).toHaveClass('peer-disabled:cursor-not-allowed');
+      });
+
+      it('does not display the strikethru line', () => {
+        const props = {
+          ...textWithImageProps,
+          disabled: true,
+          disabledBanner: 'Bruce'
+        };
+
+        const { queryByTestId } = render(MegaButton, {
+          props
+        });
+
+        const strikethruBox = queryByTestId('strikethru');
+        expect(strikethruBox).toBeNull;
+      });
+
+      it('displays the standard banner box', () => {
+        const props = {
+          ...textWithImageProps,
+          disabled: true,
+          disabledBanner: 'Bruce'
+        };
+
+        const { queryByTestId } = render(MegaButton, {
+          props
+        });
+
+        const disabledBanner = queryByTestId('disabledBanner');
+        expect(disabledBanner).toBeNull;
+
+        const imageDisabledBanner = queryByTestId('imageDisabledBanner');
+        expect(imageDisabledBanner).toHaveTextContent(props.disabledBanner);
+      });
+
+    });
+
+  });
+
 });
