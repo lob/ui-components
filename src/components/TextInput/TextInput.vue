e4d0e060950d369a0c54285df891321a6ba9d8eb
Shanna Murry <shanna.murry84@gmail.com>
2022-05-18 14:44:24 -0400

    added error styling (border and background color)

--- src/components/TextInput/TextInput.vue
+++ src/components/TextInput/TextInput.vue
@@ -45,7 +45,7 @@
         {'flex items-center': !selectedOptions},
         {'!border-0': withCopyButton},
         {'hover:shadow': !disabled && !readonly},
-        {'border-error': error}
+        {'!border-coral-700': error}
       ]"
     >
       <div
@@ -69,7 +69,7 @@
           {'!pl-3 !pr-3 !py-2': small},
           {'border border-r-0 border-gray-100 rounded-tr-none rounded-br-none truncate': withCopyButton},
           {'bg-white-300 cursor-not-allowed': disabled || readonly},
-          {'border-error': error}
+          {'!border-coral-700 bg-coral-100': error}
         ]"
         :disabled="disabled"
         :required="required"
