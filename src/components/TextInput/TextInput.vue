4f407ff292ae384177a51b7b75dd57807179405f
Shanna Murry <shanna.murry84@gmail.com>
2022-05-19 11:19:24 -0400

    fixes autofill of input changing background color

--- src/components/TextInput/TextInput.vue
+++ src/components/TextInput/TextInput.vue
@@ -262,3 +262,13 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 30px white inset !important;
+    box-shadow: 0 0 0 30px white inset !important;
+}
+</style>
