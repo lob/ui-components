caa1f4d1edf7a965b16a958de8f707a149642909
Shanna Murry <shanna.murry84@gmail.com>
2022-05-20 09:49:03 -0400

    fixed indentation linting error :(

--- src/components/TextInput/TextInput.vue
+++ src/components/TextInput/TextInput.vue
@@ -291,7 +291,7 @@ input:-webkit-autofill,
 input:-webkit-autofill:hover,
 input:-webkit-autofill:focus,
 input:-webkit-autofill:active {
-    -webkit-box-shadow: 0 0 0 30px white inset !important;
-    box-shadow: 0 0 0 30px white inset !important;
+  -webkit-box-shadow: 0 0 0 30px white inset !important;
+  box-shadow: 0 0 0 30px white inset !important;
 }
 </style>
