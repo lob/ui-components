5018cdf11499f1ec74fb10b4c570e1567d3ae130
Mocha B <76072321+BrownMY@users.noreply.github.com>
2022-05-23 08:18:28 -0700

    plus sign corrected

--- src/components/Icons/Plus.vue
+++ src/components/Icons/Plus.vue
@@ -1,17 +1,19 @@
 <template>
   <svg
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M11.5 5V19"
+      d="M12 5V19"
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
-      d="M5 12.5H19"
+      d="M5 12H19"
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
