d9e9dffeff330404ee2e7e3e317fc9c7938abd1e
Shanna Murry <shanna.murry84@gmail.com>
2022-05-26 12:29:40 -0400

    stories wip

--- src/components/RadioButtonLarge/RadioButtonLarge.stories.js
+++ src/components/RadioButtonLarge/RadioButtonLarge.stories.js
@@ -8,14 +8,6 @@ export default {
     docs: {
       page: mdx
     }
-  },
-  argTypes: {
-    size: {
-      options: ['default', 'large'],
-      control: {
-        type: 'select'
-      }
-    }
   }
 };
 
@@ -32,4 +24,20 @@ Primary.args = {
   id: '4x6',
   label: '4x6',
   value: '4x6'
+};
+
+const WithHelperTextTemplate = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { RadioButtonLarge },
+  setup: () => ({ args }),
+  template: '<radio-button-large v-bind="args" />'
+});
+
+export const WithHelperText = WithHelperTextTemplate.bind({});
+WithHelperText.args = {
+  name: 'postcard-size',
+  id: '4x6',
+  label: '4x6',
+  value: '4x6',
+  helperText: 'Helper text'
 };
