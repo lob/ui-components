dc72f495f87a211e1af312660322f87460fb3216
Daniel Murga <daniel.murga@lob.com>
2022-05-31 06:37:45 -0700

    DANG-1226 Icon updates col 1&2 (#262)
    * rebase on feature branch
    
    * remove Icons that have no use yet - as per the icons sheet
    
    * replace #416581 with currentColor
    
    Co-authored-by: Myrtle <myrtle.tzouma@lob.com>

---
+++ src/components/Icons/InfoFilled.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="9"
+      fill="#5F83A5"
+    />
+    <path
+      d="M12.302 7C11.574 7 11 7.546 11 8.274C11 9.002 11.574 9.548 12.302 9.548C13.03 9.548 13.618 9.002 13.618 8.274C13.618 7.546 13.03 7 12.302 7ZM13.45 17.416V11.276L11.168 12.276V17.416H13.45Z"
+      fill="white"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'InfoFilled'
+};
+</script>
