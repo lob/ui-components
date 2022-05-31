396db87851849670ff21ae5cb49912593e5de013
Mocha Brown <76072321+BrownMY@users.noreply.github.com>
2022-05-31 09:11:01 -0400

    Updated regular icons columns 3&4 (#259)
    * Updated regular icons columns 3&4
    
    * changing version back down to 196
    
    * linted and changed fill color from fixed to 'currentColor'
    
    * linting
    
    * removed instances of ui-components as dependencies
    
    * ran npm i to fix unnessary code blocks here
    
    * plus sign corrected
    
    * Removal of unused new icons
    
    * remove unused icons continue - rebase & fix conflicts
    
    Co-authored-by: Myrtle <myrtle.tzouma@lob.com>

--- src/components/Icons/Plus.vue
+++ src/components/Icons/Plus.vue
@@ -1,13 +1,22 @@
 <template>
   <svg
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    class="fill-current"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5Z"
+      d="M12 5V19"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5 12H19"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 </template>
