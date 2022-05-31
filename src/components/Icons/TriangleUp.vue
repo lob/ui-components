a108ad6937eeee7d5d3c4542188e4dbaf67cee8f
Myrtle <myrtle.tzouma@lob.com>
2022-05-31 10:14:08 -0400

    add new triangles/carets

--- src/components/Icons/TriangleUp.vue
+++ src/components/Icons/TriangleUp.vue
@@ -1,12 +1,21 @@
 <template>
   <svg
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    class="fill-current"
+    stroke-width="1"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M11.383 9.42332C11.5809 9.12652 12.0171 9.12652 12.215 9.42332L15.2808 14.022C15.5023 14.3542 15.2641 14.7993 14.8647 14.7993L8.73327 14.7993C8.33392 14.7993 8.09572 14.3542 8.31724 14.022L11.383 9.42332Z"
+      stroke="none"
+      d="M0 0h24v24H0z"
+      fill="none"
     />
+    <path d="M18 15l-6 -6l-6 6h12" />
   </svg>
 </template>
 
