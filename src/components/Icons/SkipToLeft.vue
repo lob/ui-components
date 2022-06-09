2bc14e8bd67e5d54a9a9495bf39d92a8f11a2082
Myrtle <myrtle.tzouma@lob.com>
2022-06-09 10:45:06 -0400

    replace Skip icons with the ones from tabler - for consistency

--- src/components/Icons/SkipToLeft.vue
+++ src/components/Icons/SkipToLeft.vue
@@ -1,20 +1,26 @@
 <template>
   <svg
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
+    stroke-width="1"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
     xmlns="http://www.w3.org/2000/svg"
-    class="fill-current"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L11.2929 12.7071C10.9024 12.3166 10.9024 11.6834 11.2929 11.2929L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289Z"
+      stroke="none"
+      d="M0 0h24v24H0z"
+      fill="none"
     />
-    <rect
-      x="5"
-      y="5"
-      width="2"
-      height="14"
-      rx="1"
+    <path d="M20 5v14l-12 -7z" />
+    <line
+      x1="4"
+      y1="5"
+      x2="4"
+      y2="19"
     />
   </svg>
 </template>
