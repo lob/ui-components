fab7d5c18f45f6593a24d9de8363b93ead8910aa
Myrtle <myrtle.tzouma@lob.com>
2022-06-24 13:06:20 -0400

    proposed style changes to newbutton & newlink (button variant)

--- src/components/NewLink/NewLink.vue
+++ src/components/NewLink/NewLink.vue
@@ -8,27 +8,26 @@
       :aria-disabled="disabled"
       :class="[
         { 'pointer-events-none': disabled },
-        { 'px-0 text-base ': link },
+        { 'px-0 text-base': link },
         { 'font-bold': bold },
         { 'underline': underline && link },
         { '!text-gray-500': disabled && link },
         { 'text-inherit': inheritTextColor },
-        { 'text-primary-500 hover:text-primary-900 active:text-primary-900': !inheritTextColor },
-        { 'flex justify-center items-center rounded-lg no-underline tracking-[-0.04em] font-bold': primary || secondary },
+        { 'text-primary-500 hover:text-primary-900 active:text-primary-900': !inheritTextColor && link },
+        { 'flex justify-center items-center rounded-lg no-underline font-medium': primary || secondary },
         { 'focus-visible:ring-4 focus:outline-none': primary || secondary },
         { 'focus:ring-primary-100': !warning && (primary || secondary) },
         { 'focus:ring-coral-700': warning && (primary || secondary) },
-        { 'px-8 text-[1.25rem] h-[48px]': regular && (primary || secondary) },
-        { 'px-6 text-sm h-[32px]': small && (primary || secondary) },
+        { 'px-6 text-base h-[48px]': regular && (primary || secondary) },
+        { 'px-4 text-sm h-[32px]': small && (primary || secondary) },
         { 'primary bg-primary-500 !text-white active:bg-black': !disabled && primary && !warning },
         { 'bg-gray-100 !text-white': disabled && primary && !warning },
         { 'primary warning bg-coral-900 !text-white active:bg-coral-700': !disabled && primary && warning },
         { 'bg-coral-200 !text-white': disabled && primary && warning },
-        { 'secondary border border-primary-500 bg-white text-primary-500': !disabled && secondary && !warning,
-          'focus:border-primary-500 active:border-black active:text-black': !disabled && secondary && !warning },
+        { 'secondary border border-[#5F83A5] text-[#416581] hover:bg-[#c5d6e626]': !disabled && secondary && !warning,
+          'active:bg-[#c5d6e640] active:border-black active:text-black': !disabled && secondary && !warning },
         { 'border border-gray-100 text-gray-100': disabled && secondary && !warning },
-        { 'secondary border border-coral-900 bg-white !text-coral-900': !disabled && secondary && warning,
-          'focus:border-coral-900 active:border-coral-700 active:!text-coral-700': !disabled && secondary && warning },
+        { 'secondary border bg-white border-[#DE3124] text-[#DE3124] hover:bg-[#de31240a] active:bg-[#de312414]': !disabled && secondary && warning },
         { 'border border-coral-200 text-coral-200': disabled && secondary && warning }
       ]"
       v-bind="$attrs"
@@ -125,28 +124,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.primary:not(:disabled):not(:active) {
+  background: linear-gradient(114.08deg, #1876db 7.95%, #5748ff 90.87%);
+}
+
 .primary:focus:not(:active):not(:disabled),
 .primary:hover:not(:disabled):not(:focus) {
-  background: linear-gradient(105.01deg, #0154ac 17.25%, #1876db 93.21%);
+  background: linear-gradient(114.08deg, #5748ff 7.95%, #1876db 90.87%);
+}
+
+.primary.warning:not(:disabled):not(:active) {
+  background: linear-gradient(114.08deg, #DB1818 7.95%, #EC4949 90.87%);
 }
 
 .primary.warning:focus:not(:active):not(:disabled),
 .primary.warning:hover:not(:disabled):not(:focus) {
-  background: linear-gradient(95.27deg, #611d18 4.76%, #943832 81.28%);
-}
-
-.secondary:hover:not(:disabled):not(:focus),
-.primary:hover:not(:disabled):not(:focus) {
-  box-shadow:
-    0 9px 12px 0 rgba(0, 0, 0, 0.2),
-    0 19px 29px 0 rgba(0, 0, 0, 0.14),
-    0 7px 36px 0 rgba(0, 0, 0, 0.12);
-}
-
-.primary:not(:disabled):focus:active,
-.secondary:not(:disabled):focus:active,
-.primary:not(:disabled):focus:not(:focus-visible),
-.secondary:not(:disabled):focus:not(:focus-visible) {
-  box-shadow: none;
+  background: linear-gradient(114.08deg, #EC4949 7.95%, #DB1818 90.87%);
 }
 </style>
