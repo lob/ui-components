0342179fad8233fe245269c29e7410c42fa9084b
Myrtle <myrtle.tzouma@lob.com>
2022-06-28 14:27:09 -0400

    updates TextInput hover & focus style

--- src/components/TextInput/TextInput.vue
+++ src/components/TextInput/TextInput.vue
@@ -44,13 +44,13 @@
       :class="[
         'bg-white h-12 pl-4 pr-4 py-2.5 rounded-lg flex items-center gap-2 border border-gray-100 focus-within:outline-none',
         { '!pl-3 !pr-3 !h-8 !gap-1' : small },
-        {'hover:shadow focus-within:shadow focus-within:ring-1 focus-within:ring-primary-500 focus-within:border-primary-500': !disabled && !readonly},
+        {'hover:border-gray-300 focus-within:ring-1 focus-within:ring-primary-500 focus-within:border-primary-500': !disabled && !readonly},
         {'!bg-white-100' : disabled},
         {'!bg-white-100 focus-within:ring-1 focus-within:ring-primary-500 focus-within:border-primary-500' : readonly},
         {'!border-coral-700 !bg-coral-100 focus-within:ring-1 focus-within:ring-coral-700': error},
         {'!bg-white-100' : withCopyButton},
         {'!flex-wrap !h-fit' : isMultiselect},
-        {'border-gray-500 focus-within:border-primary-500' : modelValue && !error && !withCopyButton}
+        {'focus-within:border-primary-500' : modelValue && !error && !withCopyButton}
       ]"
     >
       <div
@@ -74,7 +74,7 @@
           {'text-xs': small},
           {'truncate': withCopyButton},
           {'bg-white-100 cursor-not-allowed !text-gray-100 !placeholder-gray-100': disabled || readonly},
-          {'bg-coral-100 !placeholder-error !text-error !autofill:bg-coral-100 errorAutofill': error}
+          {'bg-coral-100 !placeholder-error !autofill:bg-coral-100 errorAutofill': error}
         ]"
         :disabled="disabled"
         :required="required"
