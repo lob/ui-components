f46e540e598ae40b227a02305b08fbddd8309855
Myrtle <myrtle.tzouma@lob.com>
2022-07-06 14:51:23 -0400

    fix the Colors story so that all colors display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ['./src/**/*.vue'],
+  safelist: [
+    {
+      pattern: /text-(transparent|current|black|success|lavender|papaya)/
+    },
+    {
+      pattern: /text-(primary|secondary|lemon|turquoise|flint|mint|coral|gray|white)-(100|300|500|700|900)/
+    }
+  ],
   plugins: [
     require('tailwind-plugin-lob')
   ]
