753d4c89bbc44277dc4b6653101c1612acd7d8f4
Myrtle <50080618+Mrtly@users.noreply.github.com>
2022-07-07 08:52:50 -0400

    DANG-1393 / fix the Colors story so that all colors display (#305)
    * fix the Colors story so that all colors display
    
    * add 200 to whitelist pattern as white uses it
    
    * remove transparent & current from whitelist as they are not used in the story now

--- src/theme/colors/Colors.vue
+++ src/theme/colors/Colors.vue
@@ -3,14 +3,13 @@
     <div
       v-for="color in Object.keys(themeColors)"
       :key="color"
-      class="pb-8"
+      class="mb-5"
     >
-      <h1
-        :class="`text-${color}`"
-      >
-        {{ color }}
-      </h1>
-
+      <div v-if="typeof themeColors[color] !== 'object'">
+        <h2 :class="`text-${color}`">
+          {{ color }}
+        </h2>
+      </div>
       <template
         v-if="typeof themeColors[color] === 'object'"
       >
@@ -18,12 +17,12 @@
           v-for="nestedColor in Object.keys(themeColors[color])"
           :key="nestedColor"
         >
-          <h1
+          <h2
             v-if="typeof themeColors[color][nestedColor] === 'string' && nestedColor !== 'DEFAULT'"
             :class="`text-${color}-${nestedColor}`"
           >
             {{ color }}-{{ nestedColor }} ({{ theme.colors[color][nestedColor].toUpperCase() }})
-          </h1>
+          </h2>
         </div>
       </template>
     </div>
@@ -40,6 +39,8 @@ export default {
   name: 'Theme',
   computed: {
     themeColors () {
+      delete colors.transparent;
+      delete colors.current;
       return colors;
     },
     theme () {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ['./src/**/*.vue'],
+  safelist: [
+    {
+      pattern: /text-(black|warning|error|success|lavender|chili|papaya)/
+    },
+    {
+      pattern: /text-(primary|secondary|lemon|turquoise|flint|mint|coral|gray|white)-(100|200|300|500|700|900)/
+    }
+  ],
   plugins: [
     require('tailwind-plugin-lob')
   ]
