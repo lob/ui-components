9fdbf65c50cc65d09689ce57f157d4e01d712fb3
Myrtle <myrtle.tzouma@lob.com>
2022-07-12 13:41:30 -0400

    fix modal test with slots

--- src/components/Modal/__tests__/Modal.spec.js
+++ src/components/Modal/__tests__/Modal.spec.js
@@ -7,23 +7,22 @@ const renderComponent = (options) => render(Modal, { ...options });
 describe('Modal', () => {
 
   it('is hidden by default', () => {
-    const { queryByRole } = renderComponent();
+    const slots = { header: 'modal header' };
+    const { queryByRole } = renderComponent({ slots });
 
     const modal = queryByRole('dialog');
     expect(modal).not.toBeInTheDocument();
-
   });
 
   it('is visible when its visible prop is true', () => {
     const props = {
       visible: true
     };
-
-    const { queryByRole } = renderComponent({ props });
+    const slots = { header: 'modal header' };
+    const { queryByRole } = renderComponent({ slots, props });
 
     const modal = queryByRole('dialog');
     expect(modal).toBeInTheDocument();
-
   });
 
 });
