b43717a216d86ce56ed5d9f67378946ea637d4f1
Myrtle <myrtle.tzouma@lob.com>
2022-11-10 10:49:33 -0500

    MainNav links color from gray-500 to 600

--- src/components/MainNavigation/MainNavigationChildItem.vue
+++ src/components/MainNavigation/MainNavigationChildItem.vue
@@ -1,14 +1,14 @@
 <template>
   <li
     :class="[
-      'w-full no-underline my-1 max-h-12 flex items-center font-light text-sm text-left text-gray-500 hover:text-primary-700',
+      'w-full no-underline my-1 max-h-12 flex items-center font-light text-sm text-left text-gray-600 hover:text-primary-700',
       { 'font-medium bg-white-300 rounded-l-full ring-2 ring-primary-100': active }
     ]"
     data-testid="nav-child-item"
   >
     <LobLink
       :to="to"
-      class="block w-44 rounded-l-full py-2 pl-4 !text-sm whitespace-nowrap overflow-hidden !text-gray-500 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-primary-100"
+      class="block w-44 rounded-l-full py-2 pl-4 !text-sm whitespace-nowrap overflow-hidden !text-gray-600 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-primary-100"
       :underline="false"
       @click.stop="handleNavigation"
     >
--- src/components/MainNavigation/MainNavigationItem.vue
+++ src/components/MainNavigation/MainNavigationItem.vue
@@ -3,7 +3,7 @@
     <component
       :is="tag"
       :class="[
-        'no-underline py-3 !px-6 max-h-12 flex items-center w-full font-light whitespace-nowrap !text-sm text-left !text-gray-500 relative overflow-hidden hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent'
+        'no-underline py-3 !px-6 max-h-12 flex items-center w-full font-light whitespace-nowrap !text-sm text-left !text-gray-600 relative overflow-hidden hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent'
       ]"
       :to="to"
       :underline="false"
