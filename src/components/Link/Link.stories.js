356c2de611ebac8872e74437acec0d92ca99d02c
Myrtle <myrtle.tzouma@lob.com>
2022-11-23 14:39:08 -0500

    add button styles to link

--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -23,8 +23,20 @@ export default {
     }
   },
   argTypes: {
+    content: {
+      control: {
+        type: 'text'
+      },
+      description: 'Link display name',
+      table: {
+        defaultValue: 'This is a link',
+        type: {
+          summary: 'html or component'
+        }
+      }
+    },
     variant: {
-      options: ['link', 'primary-button', 'secondary-button'],
+      options: ['link', 'primary-button', 'secondary-button', 'quiet-button', 'ghost-button', 'danger-button', 'danger-secondary-button'],
       control: {
         type: 'select'
       }
@@ -57,17 +69,21 @@ const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { LobLink },
   setup: () => ({ args }),
-  template: '<lob-link v-bind="args" to="/internal">Click me</lob-link>'
+  template: '<lob-link v-bind="args" to="/internal">{{ args.content }}</lob-link>'
 });
 
-export const RegularLink = Template.bind({});
-
+export const DefaultLink = Template.bind({});
+DefaultLink.args = {
+  content: 'I am a link'
+};
 export const StyledAsPrimaryButton = Template.bind({});
 StyledAsPrimaryButton.args = {
-  variant: 'primary-button'
+  variant: 'primary-button',
+  content: 'I look like a button'
 };
 
 export const StyledAsSecondaryButton = Template.bind({});
 StyledAsSecondaryButton.args = {
-  variant: 'secondary-button'
+  variant: 'secondary-button',
+  content: 'I look like a button'
 };
--- src/components/Link/Link.vue
+++ src/components/Link/Link.vue
@@ -12,7 +12,22 @@
                  { 'underline': link && underline },
                  link ? `type-${size}-600`: '',
                  link && bold ? `type-${size}-800 no-underline`: '',
-                 { '!text-gray-400': link && disabled }
+                 { 'flex justify-center items-center rounded-full focus:outline-dashed focus:outline-black focus:outline-offset-1 focus:ring-0': isButtonVariant },
+                 { 'type-xs-700 py-2 px-4': isButtonVariant && size==='small' },
+                 { 'type-small-700 py-2 px-4': isButtonVariant && size==='base' },
+                 { 'type-small-700 py-3 px-5': isButtonVariant && size==='large' },
+                 { 'type-base-700 py-[14px] px-6': isButtonVariant && size==='xl' },
+                 { '!text-gray-400': link && disabled },
+                 { 'bg-black text-white hover:bg-gray-700 active:bg-gray-800 focus:bg-gray-800' : primaryButton },
+                 { 'bg-white text-gray-800 border border-gray-800 hover:bg-gray-50 active:bg-gray-100 focus:bg-gray-100' : secondaryButton },
+                 { '!border-gray-200 !text-gray-400': secondaryButton && disabled },
+                 { 'bg-gray-50 text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-600 hover:border-gray-400 active:bg-gray-100 active:text-gray-600 active:border-gray-500 focus:bg-gray-100 focus:text-gray-600' : quietButton },
+                 { 'bg-transparent text-gray-500 hover:bg-gray-100 hover:text-gray-600 active:bg-gray-200 active:text-gray-600 focus:bg-gray-100 focus:text-gray-600' : ghostButton },
+                 { '!bg-gray-100 !text-gray-300 !border-none': (primaryButton || quietButton || ghostButton) && disabled },
+                 { 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700 focus:bg-red-600' : dangerButton },
+                 { '!bg-red-200 !text-red-400': dangerButton && disabled },
+                 { 'bg-white text-red-500 border border-red-500 hover:bg-red-50 hover:border-red-600 hover:text-red-600 active:bg-red-100 active:border-red-700 active:text-red-700 focus:bg-red-50 focus:border-red-600 focus:text-red-600' : dangerSecondaryButton },
+                 { '!border-red-300 !text-red-300': dangerSecondaryButton && disabled }
       ]"
     >
       <slot />
@@ -35,7 +50,7 @@ export default {
       type: String,
       default: 'link',
       validator: function (value) {
-        return ['link', 'primary-button', 'secondary-button'].includes(value);
+        return ['link', 'primary-button', 'secondary-button', 'quiet-button', 'ghost-button', 'danger-button', 'danger-secondary-button'].includes(value);
       }
     },
     size: {
@@ -83,6 +98,27 @@ export default {
     },
     linkProp () {
       return this.isExternal ? 'href' : 'to';
+    },
+    isButtonVariant () {
+      return this.variant !== 'link';
+    },
+    primaryButton () {
+      return this.variant === 'primary-button';
+    },
+    secondaryButton () {
+      return this.variant === 'secondary-button';
+    },
+    quietButton () {
+      return this.variant === 'quiet-button';
+    },
+    ghostButton () {
+      return this.variant === 'ghost-button';
+    },
+    dangerButton () {
+      return this.variant === 'danger-button';
+    },
+    dangerSecondaryButton () {
+      return this.variant === 'danger-secondary-button';
     }
   }
 };
