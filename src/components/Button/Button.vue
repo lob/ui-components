bde86a13dbb5889556b7dddc7534d63fdab66436
Myrtle <myrtle.tzouma@lob.com>
2022-11-23 14:19:37 -0500

    nits

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -9,9 +9,9 @@
       { 'cursor-not-allowed': disabled },
       { 'bg-black text-white hover:bg-gray-700 active:bg-gray-800 focus:bg-gray-800' : primary },
       { 'bg-white text-gray-800 border border-gray-800 hover:bg-gray-50 active:bg-gray-100 focus:bg-gray-100 disabled:border-gray-200 disabled:text-gray-400 disabled:hover:bg-white' : secondary },
-      { 'bg-gray-50 text-gray-500 border border-gray-200 hover:bg-gray-100 hover:text-gray-600 hover:border-gray-200 active:bg-gray-100 active:text-gray-600 active:border-gray-300' : quiet },
-      { 'bg-transparent text-gray-500 hover:bg-gray-50 hover:text-gray-600 active:bg-gray-200 active:text-gray-600 focus:bg-gray-100 focus:text-gray-600' : ghost },
-      { 'disabled:bg-gray-100 disabled:text-gray-400 disabled:border-none': primary || quiet || ghost },
+      { 'bg-gray-50 text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-600 hover:border-gray-400 active:bg-gray-100 active:text-gray-600 active:border-gray-500 focus:bg-gray-100 focus:text-gray-600' : quiet },
+      { 'bg-transparent text-gray-500 hover:bg-gray-100 hover:text-gray-600 active:bg-gray-200 active:text-gray-600 focus:bg-gray-100 focus:text-gray-600' : ghost },
+      { 'disabled:bg-gray-100 disabled:text-gray-300 disabled:border-none': primary || quiet || ghost },
       { 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700 focus:bg-red-600 disabled:bg-red-200 disabled:text-red-400' : danger },
       { 'bg-white text-red-500 border border-red-500 hover:bg-red-50 hover:border-red-600 hover:text-red-600 active:bg-red-100 active:border-red-700 active:text-red-700 focus:bg-red-50 focus:border-red-600 focus:text-red-600 disabled:border-red-300 disabled:text-red-300 disabled:hover:bg-white' : dangerSecondary },
       { '!text-blue-600 underline disabled:!text-gray-400 px-0 py-0': link }
