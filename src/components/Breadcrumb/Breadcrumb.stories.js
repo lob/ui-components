c350b182056befcfb31bc310992ffdeaa220b723
Myrtle <myrtle.tzouma@lob.com>
2022-11-30 11:54:54 -0500

    restore icon that was failing to load

--- src/components/Breadcrumb/Breadcrumb.stories.js
+++ src/components/Breadcrumb/Breadcrumb.stories.js
@@ -62,5 +62,5 @@ const Template = (args, { argTypes }) => ({
 export const Primary = Template.bind({});
 Primary.args = {
   startName: 'Dashboard',
-  iconSrc: `/${iconOverview}`
+  iconSrc: iconOverview
 };
