29171fab961fae564d8700e090a9d882107b29b1
Myrtle <myrtle.tzouma@lob.com>
2022-11-30 14:31:02 -0500

    AddressBook

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -3,3 +3,4 @@ export { default as File } from './File';
 export { default as Copy } from './Copy';
 export { default as HouseChimney } from './HouseChimney';
 export { default as ChartMixed } from './ChartMixed';
+export { default as AddressBook } from './AddressBook';
