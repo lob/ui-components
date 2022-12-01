d98c55200b66b80f7142a14eb331e617876ec66a
Myrtle <myrtle.tzouma@lob.com>
2022-12-01 15:52:57 -0500

    remaining icons skeleton

---
+++ src/components/Icons/ArrowsRotate.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg
+    :width="currentSize"
+    :height="currentSize"
+    :viewBox="`0 0 ${currentSize} ${currentSize}`"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      :d="path"
+      fill="currentColor"
+    />
+  </svg>
+</template>
+
+<script>
+const iconInfo = {
+  xxl: { size: 24, path: '' },
+  xl: { size: 20, path: '' },
+  l: { size: 18, path: '' },
+  m: { size: 16, path: '' },
+  s: { size: 14, path: '' }
+};
+
+export default {
+  name: 'ArrowsRotate',
+  props: {
+    size: {
+      type: String,
+      default: 'm',
+      validator: function (value) {
+        return ['xxl', 'xl', 'l', 'm', 's'].includes(value);
+      }
+    }
+  },
+  computed: {
+    currentSize () {
+      return iconInfo[this.size].size;
+    },
+    path () {
+      return iconInfo[this.size].path;
+    }
+  }
+};
+</script>
--- src/components/Icons/CaretDown.vue
+++ src/components/Icons/CaretDown.vue
@@ -23,7 +23,7 @@ const iconInfo = {
 };
 
 export default {
-  name: 'NewIcon',
+  name: 'CaretDown',
   props: {
     size: {
       type: String,
