9628557099329d4525dc0542f3cfada0db657d7b
Myrtle <myrtle.tzouma@lob.com>
2022-12-02 10:46:01 -0500

    remove withNestedInput story, unused functionality

--- src/components/Label/Label.stories.js
+++ src/components/Label/Label.stories.js
@@ -66,21 +66,3 @@ WithInput.args = {
   labelFor: 'Name',
   required: true
 };
-
-const WithNestedInputTemplate = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { LobLabel },
-  setup: () => ({ args }),
-  template: `
-    <lob-label v-bind="args">
-    <input class="border rounded p-2 text-gray-500" />
-    </lob-label>
-  `
-});
-
-export const WithNestedInput = WithNestedInputTemplate.bind({});
-WithNestedInput.args = {
-  label: 'Name',
-  labelFor: 'Name',
-  required: true
-};
