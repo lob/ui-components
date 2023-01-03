f3e9c8cb9bdea85f97b4d650022c435d79d7dab9
Myrtle <myrtle.tzouma@lob.com>
2023-01-03 09:52:58 -0500

    restyle Switch to Tab v2 style (black)

--- src/components/Switch/SwitchGroup.vue
+++ src/components/Switch/SwitchGroup.vue
@@ -1,11 +1,9 @@
 <template>
-  <fieldset
-    role="radiogroup"
-  >
+  <fieldset role="radiogroup">
     <legend :class="[{'text-sm font-normal normal-case tracking-normal text-gray-500 mb-2 border-b-0': !srOnlyLegend}, {'sr-only': srOnlyLegend}]">
       {{ legend }}
     </legend>
-    <div :class="['shadow flex flex-wrap p-1 bg-white rounded', { 'justify-center': center }]">
+    <div :class="['flex flex-wrap bg-white p-[1px] border border-gray-100 rounded-sm focus-within:outline-dotted focus-within:outline-black focus-within:outline-offset-1', { 'justify-center': center }]">
       <slot />
     </div>
   </fieldset>
