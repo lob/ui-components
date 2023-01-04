b7f14e59470ac49ea99c5206f2dc74bbd01787ec
Myrtle <myrtle.tzouma@lob.com>
2023-01-04 15:28:05 -0500

    focus visible nit

--- src/components/TopNavDropdown/TopNavDropdownItem.vue
+++ src/components/TopNavDropdown/TopNavDropdownItem.vue
@@ -5,7 +5,7 @@
     :target="target"
     :disabled="disabled"
     :class="['flex flex-nowrap items-center no-underline py-2 px-4 type-small-500 hover:bg-gray-50',
-             { 'focus-visible:rounded-sm focus-visible:bg-gray-50 focus:outline-black focus:outline-dashed focus:!ring-none': !disabled },
+             { 'focus-visible:rounded-sm focus-visible:bg-gray-50 focus-visible:outline-black focus-visible:outline-dashed focus-visible:!ring-none': !disabled },
              { '!text-gray-500 hover:!text-gray-800 focus-visible:!text-gray-800 active:bg-black active:!text-white': !disabled },
              { '!text-gray-300 focus:!outline-none': disabled }]"
   >
