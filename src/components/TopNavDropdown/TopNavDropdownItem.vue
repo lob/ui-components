25f92a64eba8775ba79998b596ae2a1df1ddfe4e
Myrtle <myrtle.tzouma@lob.com>
2023-01-04 10:36:41 -0500

    remove unused disabled class from item

--- src/components/TopNavDropdown/TopNavDropdownItem.vue
+++ src/components/TopNavDropdown/TopNavDropdownItem.vue
@@ -6,7 +6,7 @@
     :disabled="disabled"
     :class="['flex flex-nowrap items-center no-underline py-2 px-4 type-small-500 hover:bg-gray-50',
              { 'focus-visible:rounded-sm focus-visible:bg-gray-50 focus:outline-black focus:outline-dashed focus:!ring-none': !disabled },
-             { '!text-gray-500 disabled:!text-gray-300 hover:!text-gray-800 focus-visible:!text-gray-800 active:bg-black active:!text-white': !disabled },
+             { '!text-gray-500 hover:!text-gray-800 focus-visible:!text-gray-800 active:bg-black active:!text-white': !disabled },
              { '!text-gray-300 focus:!outline-none': disabled }]"
   >
     <div :class="{ 'mr-4': hasIcon }">
