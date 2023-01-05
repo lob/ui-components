8dfc4d7383f7738b6436dd596b50837297f0ece5
Myrtle <myrtle.tzouma@lob.com>
2023-01-05 15:20:07 -0500

    add shadow!

--- src/components/Alert/Alert.vue
+++ src/components/Alert/Alert.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="`w-full rounded-lg py-4 px-6 ${variantDetails.color} ${variantDetails.bgColor}`"
+    :class="`w-full rounded-lg shadow-XLarge py-4 px-6 ${variantDetails.color} ${variantDetails.bgColor}`"
     data-testid="alert"
   >
     <div
