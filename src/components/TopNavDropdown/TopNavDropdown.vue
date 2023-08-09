c4d7a0103597600036688fdcea642f59133f3845
Ken Crimmins <kencrim@gmail.com>
2023-08-09 16:13:31 -0700

    Add optional icon prop to top nav dropdown

--- src/components/TopNavDropdown/TopNavDropdown.vue
+++ src/components/TopNavDropdown/TopNavDropdown.vue
@@ -11,8 +11,10 @@
   >
     <div
       :id="dropdownToggleId"
-      :class="['inline-block cursor-pointer w-auto h-[76px] py-7 px-8',
-               'transition-colors duration-200 ease-linear hover:bg-gray-50 focus:outline-black focus:outline-dashed focus:outline-1']"
+      :class="[
+        'inline-block cursor-pointer w-auto h-[76px] py-7 px-8',
+        'transition-colors duration-200 ease-linear hover:bg-gray-50 focus:outline-black focus:outline-dashed focus:outline-1'
+      ]"
       tabindex="0"
       :aria-controls="dropdownListId"
       aria-haspopup="menu"
@@ -21,27 +23,39 @@
         ref="titleItem"
         class="flex items-center type-small-700 text-gray-500 whitespace-nowrap"
       >
+        <component
+          class="m-0 p-0"
+          :is="icon"
+          v-if="icon"
+          v-bind="iconProps"
+        ></component>
         {{ title }}
         <ChevronDown
-          v-if=" showChevron"
+          v-if="showChevron"
           size="s"
-          :class="['ml-2 -mr-2 transition-transform duration-200 ease-linear',
-                   { '-rotate-180': showNav }]"
+          :class="[
+            'ml-2 -mr-2 transition-transform duration-200 ease-linear',
+            { '-rotate-180': showNav }
+          ]"
         />
       </div>
     </div>
     <nav
       :id="dropdownListId"
       :aria-labelledby="dropdownToggleId"
-      :class="['min-w-full bg-white absolute',
-               { 'top-[76px] !block border-t-2 border-gray-50': showNav },
-               { '!hidden': !showNav },
-               { 'right-0': right }]"
+      :class="[
+        'min-w-full bg-white absolute',
+        { 'top-[76px] !block border-t-2 border-gray-50': showNav },
+        { '!hidden': !showNav },
+        { 'right-0': right }
+      ]"
     >
       <div
         ref="dropdownMenu"
-        :class="['p-4 h-auto w-full mt-1 opacity-100',
-                 { 'md:shadow-large bg-white': showNav }]"
+        :class="[
+          'p-4 h-auto w-full mt-1 opacity-100',
+          { 'md:shadow-large bg-white': showNav }
+        ]"
       >
         <slot />
       </div>
@@ -74,44 +88,60 @@ export default {
     showChevron: {
       type: Boolean,
       default: true
+    },
+    icon: {
+      type: [Object, null],
+      default: null
+    },
+    iconProps: {
+      type: Object,
+      default: {}
     }
   },
   emits: ['click'],
-  data () {
+  data() {
     return {
       showNav: false
     };
   },
   computed: {
-    dropdownToggleId () {
+    dropdownToggleId() {
       return `dropdown-toggle-${this.id}`;
     },
-    dropdownListId () {
+    dropdownListId() {
       return `dropdown-list-${this.id}`;
     }
   },
   methods: {
-    onKeydown ($event) {
+    onKeydown($event) {
       const anchorTags = this.$refs.dropdownMenu.getElementsByTagName('a');
       const firstAnchorTag = anchorTags[0];
       const lastAnchorTag = anchorTags[anchorTags.length - 1];
 
-      if ($event.key === 'Tab' && $event.shiftKey && $event.target === firstAnchorTag) {
+      if (
+        $event.key === 'Tab' &&
+        $event.shiftKey &&
+        $event.target === firstAnchorTag
+      ) {
         this.onBlur();
       }
 
-      if ($event.key === 'Tab' && !$event.shiftKey && $event.target === lastAnchorTag) {
+      if (
+        $event.key === 'Tab' &&
+        !$event.shiftKey &&
+        $event.target === lastAnchorTag
+      ) {
         this.onBlur();
       }
     },
-    onBlur () {
+    onBlur() {
       this.showNav = false;
     },
-    onEscape () {
+    onEscape() {
       this.$refs.titleItem.focus();
       this.showNav = false;
     },
-    onClick ($event) {
+    onClick($event) {
       this.showNav = !this.showNav;
       this.$emit('click', $event);
     }
