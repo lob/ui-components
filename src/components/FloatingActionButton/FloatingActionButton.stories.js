098d62b3f4a06e70dea3e40aa83d89f0555be709
Mike Morgan <michael.morgan@lob.com>
2024-03-05 13:36:34 -0800

    GC-2490 create FAB component

---
+++ src/components/FloatingActionButton/FloatingActionButton.stories.js
@@ -0,0 +1,71 @@
+import FloatingActionButton from './FloatingActionButton.vue';
+import mdx from './FloatingActionButton.mdx';
+
+export default {
+  title: 'Components/FloatingActionButton',
+  component: FloatingActionButton,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  },
+  argTypes: {
+    labelText: {
+      control: {
+        type: 'text'
+      },
+      description: 'Label text to display inside of the FloatingActionButton',
+      table: {
+        defaultValue: 'I am a FloatingActionButton.',
+        type: {
+          summary: 'string'
+        }
+      }
+    },
+    buttonText: {
+      control: {
+        type: 'text'
+      },
+      description: 'Content to display inside of the FloatingActionButton',
+      table: {
+        defaultValue: 'Submit',
+        type: {
+          summary: 'string'
+        }
+      }
+    },
+    buttonVariant: {
+      control: {
+        type: 'text'
+      },
+      description:
+        'Variant to make the button within the FloatingActionButton. Any value from the `Button` component is valid',
+      table: {
+        defaultValue: 'primary',
+        type: {
+          summary: 'string'
+        }
+      }
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FloatingActionButton },
+  setup: () => ({ args }),
+  template: `
+  <div style="max-height: 200px; overflow-y: auto;">
+    <div style="height: 5000px;">
+      <floating-action-button v-bind="args"></floating-action-button>
+    </div>
+  </div>
+`
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  labelText: 'I am a FloatingActionButton.',
+  buttonText: 'Submit',
+  buttonVariant: 'primary'
+};
