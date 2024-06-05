98c14f82ed4899c7eca2e649fa68457f4fef72d9
Nathaniel Waldschmidt <77284592+NateWaldschmidt@users.noreply.github.com>
2024-06-05 15:26:17 -0500

    feat: grid loading (#510)

--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -1,24 +1,32 @@
 <template>
-  <ul v-bind="$attrs" class="uic-grid" data-testid="uic-grid">
-    <slot />
-  </ul>
-  <Pagination
-    v-if="list"
-    data-testid="uic-grid-pagination"
-    :loading
-    :next
-    :previous
-    :total
-    :next-button-props="{ 'data-testid': 'uic-grid-pagination-next' }"
-    :previous-button-props="{ 'data-testid': 'uic-grid-pagination-previous' }"
-    @next="$emit('next')"
-    @previous="$emit('previous')"
-  />
+  <div class="uic-grid" data-testid="uic-grid">
+    <div v-if="loading" class="loading-overlay">
+      <LoadingSpinnerIcon :size="24" />
+    </div>
+
+    <ul v-bind="$attrs" class="uic-grid-list">
+      <slot />
+    </ul>
+
+    <Pagination
+      v-if="list"
+      data-testid="uic-grid-pagination"
+      :loading
+      :next
+      :previous
+      :total
+      :next-button-props="{ 'data-testid': 'uic-grid-pagination-next' }"
+      :previous-button-props="{ 'data-testid': 'uic-grid-pagination-previous' }"
+      @next="$emit('next')"
+      @previous="$emit('previous')"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Pagination } from '@/components/Pagination';
 import { IconButton } from '../IconButton';
+import { LoadingSpinnerIcon } from '@/components/LoadingSpinnerIcon';
 
 defineOptions({ inheritAttrs: false });
 
@@ -57,6 +65,21 @@ defineSlots<{
 
 <style scoped lang="scss">
 .uic-grid {
-  @apply flex flex-row flex-wrap gap-5 justify-start;
+  @apply relative;
+
+  &-list {
+    @apply flex flex-row flex-wrap gap-5 justify-start;
+    @apply min-h-[10rem];
+  }
+}
+
+.loading-overlay {
+  @apply bg-white;
+  @apply border border-gray-100;
+  @apply rounded-xl;
+  @apply shadow-xl;
+  @apply p-2;
+  @apply absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2;
+  @apply z-10;
 }
 </style>
