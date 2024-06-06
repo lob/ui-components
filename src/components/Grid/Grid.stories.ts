3bc30b70286b6b5c73eb01adf220c8159f1b721d
Nathaniel Waldschmidt <nathaniel.waldsch@gmail.com>
2024-06-06 07:30:47 -0500

    feat: improve grid responsiveness

--- src/components/Grid/Grid.stories.ts
+++ src/components/Grid/Grid.stories.ts
@@ -37,7 +37,55 @@ export const Primary: StoryObj<typeof Grid> = {
     template: `
       <Grid>
         <GridItem
-          v-for="_ in 7"
+          v-for="_ in 1"
+          src="https://images.squarespace-cdn.com/content/v1/5a28871dd74cff3b8aa5aa79/1570786491502-28VIIWBJ7OVHTN8IA46B/African+Sunset+4x3+web.jpg"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
+          src="https://images.squarespace-cdn.com/content/57ee12e62e69cf04febf10cf/1557736466257-MFXHWE3M9ZQDRHYUD7EP/Nberg24-2.jpg?format=1500w&content-type=image%2Fjpeg"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
+          src="https://s3.amazonaws.com/designs.purpletrail.com/pt5/system/352301/simple-lined-template-recipe-pages-8.5x11_352301_2_large.jpg"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
+          src="https://images.pexels.com/photos/255419/pexels-photo-255419.jpeg?cs=srgb&dl=pexels-pixabay-255419.jpg&fm=jpg"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
+          src="https://static1.squarespace.com/static/57ee12e62e69cf04febf10cf/57f239c9ebbd1aa018d061da/5cd92be2e4966b2f9bf477a4/1629722316402/Nberg24-2.jpg?format=1500w"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
+          src="bad link"
+          alt="African Sunset"
+          title="African Sunset"
+          subtitle="Postcard 6x9"
+          @click="console.log('click')"
+        />
+        <GridItem
+          v-for="_ in 1"
           src="https://images.squarespace-cdn.com/content/v1/5a28871dd74cff3b8aa5aa79/1570786491502-28VIIWBJ7OVHTN8IA46B/African+Sunset+4x3+web.jpg"
           alt="African Sunset"
           title="African Sunset"
